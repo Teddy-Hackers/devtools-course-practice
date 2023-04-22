058e3bec696952352907da564c55361041dbd4ea
mk101 <pro100maks222@gmail.com>
2023-04-22 21:41:42 +0300

    Add logic

--- modules/caesar-cipher/src/cipher.cpp
+++ modules/caesar-cipher/src/cipher.cpp
@@ -1,1 +1,77 @@
 // Copyright 2023 Kolesov Maxim
+
+#include "include/cipher.h"
+
+bool Kernel::operator==(const Kernel &k) const {
+  return
+    alphabet == k.alphabet &&
+    shift == k.shift &&
+    foreignChars == k.foreignChars;
+}
+
+bool Kernel::operator!=(const Kernel &k) const {
+  return !(*this == k);
+}
+
+CaesarCipher::CaesarCipher(const Kernel &k) {
+  std::string builder;
+
+  for (const auto &c : k.alphabet) {
+    if (builder.find(c) == std::string::npos) {
+      builder.push_back(c);
+    }
+  }
+
+  m_kernel.alphabet = builder;
+  m_kernel.shift = k.shift;
+  m_kernel.foreignChars = k.foreignChars;
+}
+
+const Kernel &CaesarCipher::getKernel() const {
+  return m_kernel;
+}
+bool CaesarCipher::operator==(const CaesarCipher &c) const {
+  return m_kernel == c.m_kernel;
+}
+bool CaesarCipher::operator!=(const CaesarCipher &c) const {
+  return m_kernel == c.m_kernel;
+}
+std::string CaesarCipher::encode(const std::string &message) const {
+  std::string builder;
+
+  for (const auto &c : message) {
+    const std::string::size_type index = m_kernel.alphabet.find(c);
+
+    if (index == std::string::npos) { // does not contain
+      if (m_kernel.foreignChars) {
+        builder.push_back(c);
+      }
+
+      continue;
+    }
+
+    builder.push_back(m_kernel.alphabet[(index + m_kernel.shift) % m_kernel.alphabet.size()]);
+  }
+
+  return builder;
+}
+std::string CaesarCipher::decode(const std::string &message) const {
+  std::string builder;
+
+  for (const auto &c : message) {
+    const std::string::size_type index = m_kernel.alphabet.find(c);
+    if (index == std::string::npos) { // does not contain
+      if (m_kernel.foreignChars) {
+        builder.push_back(c);
+      }
+
+      continue;
+    }
+
+    builder.push_back(
+        m_kernel.alphabet[std::abs(static_cast<int32_t>(index) - static_cast<int32_t>(m_kernel.shift)) % m_kernel.alphabet.size()]
+    );
+  }
+
+  return builder;
+}
--- modules/caesar-cipher/test/test_cipher.cpp
+++ modules/caesar-cipher/test/test_cipher.cpp
@@ -1,7 +1,145 @@
 // Copyright 2023 Kolesov Maxim
 
 #include <gtest/gtest.h>
+#include "include/cipher.h"
 
-TEST(Ceasar_Cipher, two_plus_two_equals_four) {
-  ASSERT_EQ(4, 2 + 2);
+TEST(CaesarCipher, assert_no_throw_in_default_constructor) {
+  ASSERT_NO_THROW(CaesarCipher cc);
+}
+
+TEST(CaesarCipher, create_cipher_with_specific_kernel) {
+  Kernel kernel;
+  kernel.alphabet = "abc";
+  kernel.shift = 7;
+
+  CaesarCipher cipher(kernel);
+
+  EXPECT_EQ(kernel, cipher.getKernel());
+}
+
+TEST(CaesarCipher, copy_cipher) {
+  Kernel kernel;
+  kernel.alphabet = "abc";
+  kernel.shift = 7;
+  CaesarCipher origin = CaesarCipher(kernel);
+
+  CaesarCipher copy = CaesarCipher(origin);
+
+  EXPECT_EQ(origin, copy);
+}
+
+TEST(CaesarCipher, kernel_equal) {
+  Kernel k, e;
+  k.alphabet = "tes";
+  k.shift = 6;
+  k.foreignChars = false;
+  e.alphabet = "tes";
+  e.shift = 6;
+  e.foreignChars = false;
+
+  bool check = k == e;
+
+  EXPECT_TRUE(check);
+}
+
+TEST(CaesarCipher, kernel_not_equal) {
+  Kernel k, e;
+  k.alphabet = "tes";
+  k.shift = 6;
+  e.alphabet = "tes";
+  e.shift = 6;
+  e.foreignChars = false;
+
+  bool check = k == e;
+
+  EXPECT_FALSE(check);
+}
+
+TEST(CaesarCipher, equal) {
+  CaesarCipher c1, c2;
+
+  bool check = c1 == c2;
+
+  EXPECT_TRUE(check);
+}
+
+TEST(CaesarCipher, not_equal) {
+  Kernel k;
+  k.shift = 42;
+  CaesarCipher c1, c2(k);
+
+  bool check = c1 == c2;
+
+  EXPECT_FALSE(check);
+}
+
+TEST(CaesarCipher, remove_equal_characters_in_constructor) {
+  Kernel k;
+  k.alphabet = "test";
+
+  CaesarCipher c(k);
+
+  EXPECT_EQ(c.getKernel().alphabet, "tes");
+}
+
+TEST(CaesarCipher, get_kernel) {
+  Kernel k;
+  k.shift = 42;
+  CaesarCipher c(k);
+
+  const Kernel &get = c.getKernel();
+
+  EXPECT_EQ(get, k);
+}
+
+TEST(CaesarCipher, encode) {
+  CaesarCipher c;
+
+  std::string result = c.encode("test");
+
+  EXPECT_EQ(result, "yjxy");
+}
+
+TEST(CaesarCipher, encode_with_space) {
+  CaesarCipher c;
+
+  std::string result = c.encode("this is a test");
+
+  EXPECT_EQ(result, "ymnx nx f yjxy");
+}
+
+TEST(CaesarCipher, encode_with_no_space) {
+  Kernel k;
+  k.foreignChars = false;
+  CaesarCipher c(k);
+
+  std::string result = c.encode("this is a test");
+
+  EXPECT_EQ(result, "ymnxnxfyjxy");
+}
+
+TEST(CaesarCipher, decode) {
+  CaesarCipher c;
+
+  std::string result = c.decode("yjxy");
+
+  EXPECT_EQ(result, "test");
+}
+
+TEST(CaesarCipher, decode_with_space) {
+  CaesarCipher c;
+
+  std::string result = c.decode("ymnx nx f yjxy");
+
+  EXPECT_EQ(result, "this is a test");
+}
+
+TEST(CaesarCipher, decode_with_no_space) {
+  Kernel k;
+  k.foreignChars = false;
+  CaesarCipher c(k);
+
+  std::string result = c.decode("ymnx nx f yjxy");
+
+  EXPECT_EQ(result, "thisisatest");
 }

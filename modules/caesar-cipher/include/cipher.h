0ea47e4221e08a84e17c7d41d4631d0affd0fb11
mk101 <pro100maks222@gmail.com>
2023-04-22 17:24:49 +0300

    Init caesar-cipher

---
+++ modules/caesar-cipher/include/cipher.h
@@ -0,0 +1,3 @@
+// Copyright 2023 Kolesov Maxim
+
+#pragma once
---
+++ modules/caesar-cipher/test/test_cipher.cpp
@@ -0,0 +1,7 @@
+// Copyright 2023 Kolesov Maxim
+
+#include <gtest/gtest.h>
+
+TEST(Ceasar_Cipher, two_plus_two_equals_four) {
+  ASSERT_EQ(4, 2 + 2);
+}

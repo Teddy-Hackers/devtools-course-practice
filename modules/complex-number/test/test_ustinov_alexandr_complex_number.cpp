74c11fc5e430d46ea7855e46551752d85229b46b
UAlex322 <epicalex2015@gmail.com>
2023-04-22 14:13:44 +0300

    Lab1: added tests for difference of complex numbers

--- modules/complex-number/test/test_ustinov_alexandr_complex_number.cpp
+++ modules/complex-number/test/test_ustinov_alexandr_complex_number.cpp
@@ -4,7 +4,7 @@
 
 #include "include/complex_number.h"
 
-TEST(Ustinov, Sum1) {
+TEST(Ustinov_ComplexNumberTest, Sum1) {
     ComplexNumber z1(1.0, 0.0);
     ComplexNumber z2(0.0, 1.0);
 
@@ -13,7 +13,7 @@ TEST(Ustinov, Sum1) {
     EXPECT_EQ(z1_plus_z2, ComplexNumber(1.0, 1.0));
 }
 
-TEST(Ustinov, Sum2) {
+TEST(Ustinov_ComplexNumberTest, Sum2) {
     ComplexNumber z1(2.0, 3.0);
     ComplexNumber z2(3.0, 5.0);
 
@@ -22,11 +22,47 @@ TEST(Ustinov, Sum2) {
     EXPECT_EQ(z1_plus_z2, ComplexNumber(5.0, 8.0));
 }
 
-TEST(Ustinov, Sum3) {
+TEST(Ustinov_ComplexNumberTest, Sum3) {
     ComplexNumber z1(0.0, 1.0);
     ComplexNumber z2(0.0, -1.0);
 
     ComplexNumber z1_plus_z2 = z1 + z2;
 
     EXPECT_EQ(z1_plus_z2, ComplexNumber(0.0, 0.0));
+}
+
+TEST(Ustinov_ComplexNumberTest, Diff1) {
+    ComplexNumber z1(1.0, 0.0);
+    ComplexNumber z2(0.0, 1.0);
+
+    ComplexNumber z1_minus_z2 = z1 - z2;
+
+    EXPECT_EQ(z1_minus_z2, ComplexNumber(1.0, -1.0));
+}
+
+TEST(Ustinov_ComplexNumberTest, Diff2) {
+    ComplexNumber z1(3.0, 1.0);
+    ComplexNumber z2(1.0, 3.0);
+
+    ComplexNumber z1_minus_z2 = z1 - z2;
+
+    EXPECT_EQ(z1_minus_z2, ComplexNumber(2.0, -2.0));
+}
+
+TEST(Ustinov_ComplexNumberTest, Diff3) {
+    ComplexNumber z1(0.0, 1.0);
+    ComplexNumber z2(0.0, 1.0);
+
+    ComplexNumber z1_minus_z2 = z1 - z2;
+
+    EXPECT_EQ(z1_minus_z2, ComplexNumber(0.0, 0.0));
+}
+
+TEST(Ustinov_ComplexNumberTest, Diff4) {
+    ComplexNumber z1(0.0, 0.0);
+    ComplexNumber z2(0.0, -0.0);
+
+    ComplexNumber z1_minus_z2 = z1 - z2;
+
+    EXPECT_EQ(z1_minus_z2, ComplexNumber(0.0, 0.0));
 }

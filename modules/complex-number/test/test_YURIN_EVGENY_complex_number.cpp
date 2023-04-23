924d1843193995349f9cb5428dffb7686c38c884
Evgeny <mousedead21@gmail.com>
2023-04-23 15:12:31 +0300

    Fixed formatting

--- modules/complex-number/test/test_YURIN_EVGENY_complex_number.cpp
+++ modules/complex-number/test/test_YURIN_EVGENY_complex_number.cpp
@@ -5,31 +5,30 @@
 #include "include/complex_number.h"
 
 TEST(Yurin_Evgeny_ComplexNumberTest, Test_Summ) {
+ ComplexNumber first(1337.0, 42.0);
+ ComplexNumber second(3.0, 8.0);
+ ComplexNumber expectedResult = ComplexNumber(1400.0, 50.0);
 
-	ComplexNumber first(1337.0, 42.0);
-	ComplexNumber second(3.0, 8.0);
-	ComplexNumber expectedResult = ComplexNumber(1400.0, 50.0);
+ ComplexNumber result = a + b;
 
-	ComplexNumber result = a + b;
-
-	ASSERT_EQ(expectedResult.getIm(), result.getIm());
-	ASSERT_EQ(expectedResult.getRe(), result.getRe());
+ ASSERT_EQ(expectedResult.getIm(), result.getIm());
+ ASSERT_EQ(expectedResult.getRe(), result.getRe());
 }
 
 TEST(Yurin_Evgeny_ComplexNumberTest, Test_minus) {
-	ComplexNumber first(1337.0, 42.0);
-	ComplexNumber second(3.0, 8.0);
-	ComplexNumber expectedResult = ComplexNumber(1334.0, 34.0);
+ ComplexNumber first(1337.0, 42.0);
+ ComplexNumber second(3.0, 8.0);
+ ComplexNumber expectedResult = ComplexNumber(1334.0, 34.0);
 
-	ComplexNumber result = a - b;
+ ComplexNumber result = a - b;
 
-	ASSERT_EQ(expectedResult.getIm(), result.getIm());
-	ASSERT_EQ(expectedResult.getRe(), result.getRe());
+ ASSERT_EQ(expectedResult.getIm(), result.getIm());
+ ASSERT_EQ(expectedResult.getRe(), result.getRe());
 }
 
 TEST(Yurin_Evgeny_ComplexNumberTest, Test_Not_Equal) {
-	ComplexNumber first(1.1, 2.1);
-	ComplexNumber second(1.0, 2.0);
-
-	EXPECT_NE(first, second);
+ ComplexNumber first(1.1, 2.1);
+ ComplexNumber second(1.0, 2.0);
+ 
+ EXPECT_NE(first, second);
 }

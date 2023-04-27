259bad1b881c73a9cd96da3863e0b832f1e60601
AleksandrNikolaev02 <alnikolaev151@yandex.ru>
2023-04-27 17:55:05 +0300

    test2

--- modules/complex-number/test/test_NIKOLAEV_ALEXANDER_complex_number.cpp
+++ modules/complex-number/test/test_NIKOLAEV_ALEXANDER_complex_number.cpp
@@ -4,15 +4,45 @@
 
 #include "include/complex_number.h"
 
-TEST(Nikolaev_Alexander_ComplexNumberTest, Can_Create_Zero) {
+TEST(Nikolaev_Alexander_ComplexNumberTest, Can_Copy_Constructor) {
     // Arrange
-    double re = 0.0;
-    double im = 0.0;
+    double re = 3.0;
+    double im = 4.0;
+    ComplexNumber a(re, im);
 
     // Act
-    ComplexNumber z(re, im);
+    ComplexNumber b = a;
 
     // Assert
-    EXPECT_EQ(re, z.getRe());
-    EXPECT_EQ(im, z.getIm());
+    EXPECT_EQ(b.getRe(), re);
+    EXPECT_EQ(b.getIm(), im);
+}
+
+TEST(Nikolaev_Alexander_ComplexNumberTest, Can_Equals) {
+    // Arrange
+    double re = 3.0;
+    double im = 4.0;
+    ComplexNumber a(re, im);
+    ComplexNumber b(re, im);
+
+    // Act
+    bool result = (a == b);
+
+    // Assert
+    ASSERT_TRUE(result);
+}
+
+TEST(Nikolaev_Alexander_ComplexNumberTest, Can_Sum) {
+    // Arrange
+    double re = 1.0;
+    double im = 2.0;
+    ComplexNumber a(re, im);
+    ComplexNumber b(re, im);
+
+    // Act
+    ComplexNumber c = a + b;
+
+    // Assert
+    ASSERT_EQ(c.getRe(), 2.0);
+    ASSERT_EQ(c.getIm(), 4.0);
 }

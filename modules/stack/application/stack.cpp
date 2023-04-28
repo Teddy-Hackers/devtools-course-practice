c10e981d949ac2ffddf06bd5603d4dd1a937e455
wellcome2AD <iuuyyttttt@mail.ru>
2023-04-28 21:08:03 +0300

    первая рабочая версия без тестов

---
+++ modules/stack/application/stack.cpp
@@ -0,0 +1,12 @@
+// Copyright 2023 Korobeynikova Alice
+
+#include <iostream>
+
+#include "include/stack_app.h"
+
+int main(int argc, const char** argv) {
+  StackApp app;
+  //char* _argv[] = {};
+  std::cout << app(argc, argv);
+  return 0;
+}
---
+++ modules/stack/src/stack_app.cpp
@@ -0,0 +1,57 @@
+// Copyright 2023 Korobeynikova Alice
+
+#include "include/stack_app.h"
+
+#include <iostream>
+#include <sstream>
+
+void StackApp::help() {
+  std::cout << "This is an integer stack application.\n\n";
+  std::cout << "Parameters:\n";
+  std::cout << "-s \t\t Output size of stack.\n";
+  std::cout << "-e \t\t Output if stack is empty.\n";
+  std::cout << "-t \t\t Output top element of stack.\n";
+  std::cout << "-pop \t\t Pop element from stack.\n";
+  std::cout << "-push <values> \t Input element(s) and push it into stack.\n";
+  std::cout << "-c \t\t Clear stack.\n\n";
+}
+
+std::string StackApp::operator()(int argc, const char** argv) {
+  if (argc == 1) {
+    help();
+  }
+
+  for (size_t i = 1; i < argc; ++i) {
+    std::string key(argv[i]);
+    if (key == "-s") {
+      message += " " + std::to_string(stack.size());
+    } else if (key == "-e") {
+      message += " " + (stack.empty() ? std::string("true") : std::string("false"));
+    } else if (key == "-t") {
+      try {
+        message += " " + std::to_string(stack.top());
+      } catch (std::out_of_range) {
+        message += "error: can't get top, stack is empty";
+      }
+    } else if (key == "-pop") {
+      try {
+        stack.pop();
+      } catch (std::out_of_range) {
+        std::cout << " error: can't pop, stack is empty";
+      }
+    } else if (key == "-push") {
+      for (++i; i < argc && *argv[i] != '-'; ++i) {
+        stack.push(atoi(argv[i]));
+      }
+      --i;
+    } else if (key == "-c") {
+      stack.clear();
+    } else {
+      if (*argv[i] != '\0') {
+        message += " error: unknown key " + key;
+      }
+    }
+  }
+  message += '\n';
+  return message;
+}

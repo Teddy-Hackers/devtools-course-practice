91ea96accd1592e0e7e80efa2f112ad1d7ede9cb
Artem Shipilov <hugo.nieto@mail.ru>
2023-04-30 00:08:57 +0300

    re-added a space at the end of the file

--- modules/complex-number/test/test_Shipilov_Artem_complex_number.cpp
+++ modules/complex-number/test/test_Shipilov_Artem_complex_number.cpp
@@ -71,4 +71,4 @@ TEST(Shipilov_Artem_ComplexNumberTest, Divsion_Of_Negative_Complex_Numbers) {
     // Assert
     EXPECT_EQ(answer_re, result.getRe());
     EXPECT_EQ(answer_im, result.getIm());
-}
+} 

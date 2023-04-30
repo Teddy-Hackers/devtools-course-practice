1ba1593385f194d07a4289439f5aa343de0a7ac5
mk101 <pro100maks222@gmail.com>
2023-04-30 22:25:48 +0300

    Add comments

--- modules/binary-search/include/search_application.h
+++ modules/binary-search/include/search_application.h
@@ -4,6 +4,12 @@
 
 #include <string>
 
+//! @addtogroup complex-number
+//! @{
+
+/**
+ * @brief Class for binary search
+ */
 class SearchApplication {
  public:
   SearchApplication() = default;
@@ -11,11 +17,30 @@ class SearchApplication {
   SearchApplication(const SearchApplication &sa) = delete;
   SearchApplication(SearchApplication &&sa) = delete;
 
+  /**
+   * @brief Functor override
+   * @param[in] argc Nuber of comand line arguments
+   * @param[in] argv Command line arguments
+   * @return Output expression
+   */
   std::string operator()(int argc, const char **argv);
+
  private:
+  /**
+   * @brief Set m_message field to help message.
+   * @param[in] appName Name of application binary
+   */
   void help(const char *appName);
 
+  /**
+   * @brief Validate console arguments
+   * @param[in] argc Nuber of comand line arguments
+   * @param[in] argv Command line arguments
+   * @return
+   */
   bool validateArguments(int argc, const char **argv);
 
   std::string m_message;
 };
+
+//! @}

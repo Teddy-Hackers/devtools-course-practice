0d08dc0e45386684550019587a2517fa982585e0
Данила Ермолаев <73434195+Dnoniel-Ermolaev@users.noreply.github.com>
2023-05-02 03:30:16 +0300

    new tests

--- modules/AVL-Tree/test/test_AVL_Tree.cpp
+++ modules/AVL-Tree/test/test_AVL_Tree.cpp
@@ -5,3 +5,50 @@
 #include "include/AVLTree.h"
 
 TEST(AVLTree, can_create_avl_tree) { ASSERT_NO_THROW(AVLTree<int> testavl;); }
+
+TEST(AVLTree, can_insert_elements) {
+  AVLTree<int> testavl;
+  ASSERT_NO_THROW(testavl.insert(10););
+}
+
+TEST(AVLTree, can_create_avl_tree_copy_constructor) {
+  AVLTree<int> testavl;
+  testavl.insert(10);
+
+  ASSERT_NO_THROW(AVLTree<int> testavl2(testavl););
+}
+
+TEST(AVLTree, can_find_element) {
+  AVLTree<int> testavl;
+  testavl.insert(10);
+  testavl.insert(20);
+  testavl.insert(12);
+  testavl.insert(15);
+  testavl.insert(16);
+  testavl.insert(18);
+
+  EXPECT_TRUE(testavl.contains(20));
+}
+TEST(AVLTree, can_find_max) {
+  AVLTree<int> testavl;
+  testavl.insert(10);
+  testavl.insert(20);
+  testavl.insert(12);
+  testavl.insert(15);
+  testavl.insert(16);
+  testavl.insert(18);
+
+  EXPECT_EQ(20, testavl.find_max());
+}
+TEST(AVLTree, can_find_min) {
+  AVLTree<int> testavl;
+  testavl.insert(10);
+  testavl.insert(20);
+  testavl.insert(12);
+  testavl.insert(15);
+  testavl.insert(16);
+  testavl.insert(18);
+
+  EXPECT_EQ(10, testavl.find_min());
+}
+

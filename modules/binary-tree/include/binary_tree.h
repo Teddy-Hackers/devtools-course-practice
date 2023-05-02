76e3b8ba25da4f96c95f0f09708f8991f33075d0
oleg-w570 <ol3g.zor1n@yandex.ru>
2023-05-03 00:38:49 +0300

    binary test app

--- modules/binary-tree/include/binary_tree.h
+++ modules/binary-tree/include/binary_tree.h
@@ -3,36 +3,34 @@
 #ifndef MODULES_BINARY_TREE_INCLUDE_BINARY_TREE_H_
 #define MODULES_BINARY_TREE_INCLUDE_BINARY_TREE_H_
 
+#include <exception>
 #include <vector>
+#include <stack>
 
 struct Node {
   int Value;
-  Node* Left;
-  Node* Right;
-  Node* Parent;
+  Node *Left;
+  Node *Right;
+  Node *Parent;
 
-  Node(const int V = 0.0, Node* P = nullptr,
-  Node* L = nullptr, Node* R = nullptr);
-  explicit Node(const Node& n);
+  explicit Node(int V = 0.0, Node *P = nullptr,
+                Node *L = nullptr, Node *R = nullptr);
+  Node(const Node &n);
 };
 
 class BinaryTree {
-  Node* root;
-  Node* curr;
-  Node* FindMin(Node* node) const;
-  Node* FindMax(Node* node) const;
-  Node* FindNext(Node* node) const;
+  Node *root;
+  Node *curr;
+  static Node *FindMin(Node *node);
+  static Node *FindMax(Node *node);
+  static Node *FindNext(Node *node);
  public:
   BinaryTree();
-  explicit BinaryTree(const int v);
-  explicit BinaryTree(const std::vector<int>& v)
-      : root(nullptr), curr(nullptr) {
-     for (const auto& value : v) {
-         Insert(value);
-     }
-  }
+  explicit BinaryTree(int v);
+  explicit BinaryTree(const std::vector<int> &v);
+  ~BinaryTree();
 
-  Node* Find(int k) const;
+  Node *Find(int k) const;
   void Insert(int k);
   void Delete(int k);
 
@@ -40,6 +38,7 @@ class BinaryTree {
   void Reset();
   void SetNext();
   bool IsEnd() const;
+  bool IsEmpty() const;
 };
 
 #endif  //  MODULES_BINARY_TREE_INCLUDE_BINARY_TREE_H_
--- modules/binary-tree/src/binary_tree.cpp
+++ modules/binary-tree/src/binary_tree.cpp
@@ -1,105 +1,138 @@
 //  Copyright 2023 Semenova Veronika
 
 #include <gtest/gtest.h>
-#include <vector>
 
 #include "include/binary_tree.h"
 
+Node::Node(const int V, Node *P,
+           Node *L, Node *R) : Value(V), Left(L),
+                               Right(R), Parent(P) {}
+Node::Node(const Node &n) :
+    Value(n.Value), Left(nullptr),
+    Right(nullptr), Parent(nullptr) {}
 
-Node::Node(const int V, Node* P,
-  Node* L, Node* R) : Value(V), Left(L),
-  Right(R), Parent(P) {}
-Node::Node(const Node& n) :
-  Value(n.Value), Left(nullptr),
-  Right(nullptr), Parent(nullptr) {}
+BinaryTree::BinaryTree() : root(nullptr), curr(nullptr) {}
 
-
-BinaryTree::BinaryTree():root(nullptr), curr(nullptr) {}
 BinaryTree::BinaryTree(int v) {
   root = new Node(v);
   curr = nullptr;
 }
-Node* BinaryTree::FindMin(Node* node) const {
-  Node* tmp = node;
-  while (tmp->Left !=
-      nullptr) {
-  tmp = tmp->Left;
+
+BinaryTree::BinaryTree(const std::vector<int> &v)
+    : root(nullptr), curr(nullptr) {
+  for (const auto &value : v) {
+    Insert(value);
+  }
 }
+
+BinaryTree::~BinaryTree() {
+  if (IsEmpty())
+    return;
+  Node *node = root;
+  std::stack<Node *> st1, st2;
+  st1.push(node);
+  while (!st1.empty()) {
+    node = st1.top();
+    st1.pop();
+    if (node->Left != nullptr)
+      st1.push(node->Left);
+    if (node->Right != nullptr)
+      st1.push(node->Right);
+    st2.push(node);
+  }
+  while (!st2.empty()) {
+    node = st2.top();
+    st2.pop();
+    delete node;
+  }
+}
+
+Node *BinaryTree::FindMin(Node *node) {
+  Node *tmp = node;
+  while (tmp->Left != nullptr) {
+    tmp = tmp->Left;
+  }
   return tmp;
 }
-void BinaryTree::Reset() {
-  if (root != nullptr) {
-    curr = FindMin(root);
-  } else {
-  throw std::string("Сan't resert");
-  }
-}
-int BinaryTree::GetValue() const {
-  if (curr != nullptr) {
-    return curr->Value;
-  } else {
-    throw std::string("Сan't set next");
-  }
-}
-void BinaryTree::Insert(int v) {
-  if (Find(v) != nullptr) {
-    throw std::string("Сan't extract a value");
-  }
-  if (root == nullptr)  {
-    root = new Node(v);
-    return;
-  }
-  Node* x = root, * y;
-  while (x != nullptr) {
-    y = x;
-    if (v < x->Value) {
-    x = x->Left;
-  } else {
-    x = x->Right;
-  }
-  }
-  if (v < y->Value) {
-    y->Left = new Node(v, y);
-  } else {
-    y->Right = new Node(v, y);
-  }
-}
-Node* BinaryTree::Find(int k)  const {
-  Node* tmp = root;
-  while ((tmp != nullptr) && (tmp->Value != k)) {
-    if (k < tmp->Value) {
-    tmp = tmp->Left;
-  } else {
+
+Node *BinaryTree::FindMax(Node *node) {
+  Node *tmp = node;
+  while (tmp->Right != nullptr) {
     tmp = tmp->Right;
   }
+  return tmp;
+}
+
+Node *BinaryTree::FindNext(Node *node) {
+  Node *res = nullptr;
+  if (node->Right != nullptr) {
+    res = FindMin(node->Right);
+    return res;
+  }
+  res = node->Parent;
+  Node *tmp = node;
+  while ((res != nullptr) && (tmp == res->Right)) {
+    tmp = res;
+    res = res->Parent;
+  }
+  return res;
+}
+
+void BinaryTree::Reset() {
+  if (IsEmpty())
+    throw std::out_of_range("Can't reset.");
+  curr = FindMin(root);
+}
+
+int BinaryTree::GetValue() const {
+  if (curr == nullptr)
+    throw std::out_of_range("Can't get.");
+  return curr->Value;
+}
+
+void BinaryTree::Insert(int v) {
+  if (Find(v) != nullptr) {
+    throw std::invalid_argument("Can't insert duplicate value.");
+  }
+
+  Node **node = &root;
+  Node *prev = root;
+  while (*node != nullptr) {
+    prev = *node;
+    Node &tmp = **node;
+    if (v < tmp.Value) {
+      node = &tmp.Left;
+    } else {
+      node = &tmp.Right;
+    }
+  }
+  *node = new Node(v, prev);
+}
+
+Node *BinaryTree::Find(int k) const {
+  Node *tmp = root;
+  while ((tmp != nullptr) && (tmp->Value != k)) {
+    if (k < tmp->Value) {
+      tmp = tmp->Left;
+    } else {
+      tmp = tmp->Right;
+    }
   }
   return tmp;
 }
-Node* BinaryTree::FindNext(Node* node) const {
-  Node* res = nullptr;
-  if (node->Right != nullptr) {
-    res = FindMin(node->Right);
-  return res;
-  }
-  res = node->Parent;
-  Node* tmp = node;
-  while ((res != nullptr) && (tmp == res->Right)) {
-    tmp = res;
-  res = res->Parent;
-  }
-  return res;
-}
+
 void BinaryTree::SetNext() {
-  if (root != nullptr) {
-    curr = FindNext(curr);
-  }
+  if (curr == nullptr)
+    throw std::out_of_range("Can't set next.");
+  curr = FindNext(curr);
 }
+
 void BinaryTree::Delete(int k) {
-  Node* z = Find(k);
+  Node *z = Find(k);
   if (Find(k) == nullptr) {
-      throw std::string("\tThe item being deleted is not in the tree\n");
+    throw std::out_of_range("The item being deleted is not in the tree.");
   }
-  Node* y = nullptr, * x = nullptr;
+  Node *y = nullptr, *x = nullptr;
   if ((z->Left != nullptr) && (z->Right != nullptr)) {
     y = FindNext(z);
   } else {
@@ -127,13 +160,11 @@ void BinaryTree::Delete(int k) {
     root = nullptr;
   }
 }
-Node* BinaryTree::FindMax(Node* node)  const {
-  Node* tmp = node;
-  while (tmp->Right != nullptr) {
-    tmp = tmp->Right;
-  }
-  return tmp;
-}
+
 bool BinaryTree::IsEnd() const {
   return curr->Value == FindMax(root)->Value;
+}
+
+bool BinaryTree::IsEmpty() const {
+  return root == nullptr;
 }

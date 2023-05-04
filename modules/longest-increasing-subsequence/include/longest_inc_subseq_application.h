dd583c4634cbff39c1fbdbaaaad0c3a0aca1cf46
FirstStep0 <75475364+FirstStep0@users.noreply.github.com>
2023-05-04 19:35:11 +0300

    Lab 2: rename test and delete extra comments

--- modules/longest-increasing-subsequence/include/longest_inc_subseq_application.h
+++ modules/longest-increasing-subsequence/include/longest_inc_subseq_application.h
@@ -5,30 +5,9 @@
 
 #include <string>
 
-/** @defgroup complex-number Complex Numbers Algebra
-  @{
-    This module contains:
-        - A definition of complex number as a class
-        - Calculator class which performs the calculations
-  @}
-*/
-
-//! @addtogroup complex-number
-//! @{
-
-/**
- * @brief Class for complex numbers algebra
- */
 class Application {
  public:
   Application();
-
-  /**
-   * @brief Functor override
-   * @param[in] argc Nuber of comand line arguments
-   * @param[in] argv Command line arguments
-   * @return Output expression
-   */
   std::string operator()(int argc, const char** argv);
 
  private:
@@ -36,7 +15,5 @@ class Application {
   bool validateNumberOfArguments(int argc, const char** argv);
   std::string message_;
 };
-
-//! @}
 
 #endif  // MODULES_LONGEST_INCREASING_SUBSEQUENCE_INCLUDE_LONGEST_INC_SUBSEQ_APPLICATION_H_

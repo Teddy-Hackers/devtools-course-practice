08a5b2554ab58fff0de845fcb6eca62d6ebfd962
AleksandrNikolaev02 <alnikolaev151@yandex.ru>
2023-05-07 22:13:41 +0300

    fix

--- modules/odd-even-merge-batcher/test/test_odd_even_merge.cpp
+++ modules/odd-even-merge-batcher/test/test_odd_even_merge.cpp
@@ -4,10 +4,14 @@
 #include "include/odd_even_merge.h"
 
 bool checkSortArray(const std::vector<int>& array) {
-    return std::is_sorted(array.begin(), array.end());
+	const int sizeArray = array.size();
+	for (int i = 0; i < sizeArray - 1; i++) {
+		if (array[i] > array[i + 1]) return false;
+	}
+	return true;
 }
 
-TEST(OddEvenMerge, can_sort_array) {
+TEST(Nikolaev_OddEvenMerge, can_sort_array) {
     // Arrange
     std::vector<int> array = {6, 5, 4, 3, 2, 1};
 
@@ -18,7 +22,7 @@ TEST(OddEvenMerge, can_sort_array) {
     ASSERT_TRUE(checkSortArray(array));
 }
 
-TEST(OddEvenMerge, can_sort_random_array) {
+TEST(Nikolaev_OddEvenMerge, can_sort_random_array) {
     // Arrange
     std::vector<int> array = getRandomArray(20);
 
@@ -29,7 +33,7 @@ TEST(OddEvenMerge, can_sort_random_array) {
     ASSERT_TRUE(checkSortArray(array));
 }
 
-TEST(OddEvenMerge, can_sort_an_array_of_negative_numbers) {
+TEST(Nikolaev_OddEvenMerge, can_sort_an_array_of_negative_numbers) {
     // Arrange
     std::vector<int> array = {-1, -2, -3, -4, -5, -6};
 

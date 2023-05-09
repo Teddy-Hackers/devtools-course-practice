b7abc32847a63f344a5eef4d832be42d51af8a45
NArKEe-ken <vadimmuxin2002@mail.ru>
2023-05-09 11:48:32 +0300

    commit

--- modules/complex-number/test/test_Mukhin_Vadim_complex_number.cpp
+++ modules/complex-number/test/test_Mukhin_Vadim_complex_number.cpp
@@ -4,7 +4,7 @@
 
 #include "include/complex_number.h"
 
-TEST(Mukhin_Vadim_ComplexNumberTest,Test3) {
+TEST(Mukhin_Vadim_ComplexNumberTest, Test3) {
     ComplexNumber t1(0.4, 0.7);
     ComplexNumber t2(0.2, 0.9);
     ComplexNumber t3 = t1 * t2;
@@ -20,7 +20,7 @@ TEST(Mukhin_Vadim_ComplexNumberTest, Test1) {
     EXPECT_DOUBLE_EQ(t1.getIm(), t2.getIm());
 }
 
-TEST(Mukhin_Vadim_ComplexNumberTest,Test2 ) {
-    ComplexNumber t1(7.0, 0.1),t2(2.0, 0.2);
+TEST(Mukhin_Vadim_ComplexNumberTest, Test2) {
+    ComplexNumber t1(7.0, 0.1), t2(2.0, 0.2);
     EXPECT_NE(t1, t2);
 }

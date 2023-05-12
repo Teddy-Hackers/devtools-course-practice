c497f2e7a924dcec1d37b864970e5d0cac0b603a
Mikerin Ilya <mikerin-2002@mail.ru>
2023-05-12 18:30:33 +0300

    tests fix

--- modules/Dijkstra/test/test_Dijkstra.cpp
+++ modules/Dijkstra/test/test_Dijkstra.cpp
@@ -93,10 +93,36 @@ TEST(Dijkstra, TEST_getPath) {
     ASSERT_EQ(d.getPathsMatrix(), path);
 }
 
+TEST(Dijkstra, TEST_Copy) {
+    Dijkstra d = Dijkstra();
+    std::vector<std::vector<double>> path = {{0,  7,  9,  -1, -1, 14},
+                                             {7,  0,  10, 15, -1, -1},
+                                             {9,  10, 0,  11, -1, 2},
+                                             {-1, 15, 11, 0,  6,  -1},
+                                             {-1, -1, -1, 6,  0,  9},
+                                             {14, -1, 2,  -1, 9,  0}};
+    Dijkstra dClone = Dijkstra(d);
+    ASSERT_EQ(dClone.getPathsMatrix(), d.getPathsMatrix());
+    ASSERT_EQ(dClone.getAnswer(), d.getAnswer());
+    ASSERT_EQ(dClone.getTaskComplite(), d.getTaskComplite());
+    d.Solve();
+    ASSERT_NE(d.getAnswer(), dClone.getAnswer());
+}
 
+TEST(Dijkstra, TEST_NoWay) {
+    Dijkstra d = Dijkstra();
+    std::vector<std::vector<double>> path = {{0 ,  -1,  -1, -1, -1, -1},
+                                             {-1 ,  0, 10, 15, -1, -1},
+                                             {-1 , 10,  0, 11, -1,  2},
+                                             {-1, 15, 11,  0,  6, -1},
+                                             {-1, -1, -1,  6,  0,  9},
+                                             {-1, -1,  2, -1,  9,  0}};
+    int ans = d.setNewTask(path, 0);
 
+    ASSERT_EQ(d.getTaskComplite(), false);
+    d.Solve();
+    ASSERT_EQ(d.getTaskComplite(), true);
 
-
-
-
-
+    ASSERT_EQ(ans, 0);
+    ASSERT_EQ(d.getAnswer(), std::vector<double>({0, -1, -1, -1, -1, -1}));
+}

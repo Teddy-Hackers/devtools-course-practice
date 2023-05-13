740fce14937442088aa45faaa632451aba98eca9
NArKEe-ken <84380106+NArKEe-ken@users.noreply.github.com>
2023-05-13 18:56:03 +0300

    commit

--- modules/Konverterandcalculator/test/test_Mukhin_Vadim_Calculator.cpp
+++ modules/Konverterandcalculator/test/test_Mukhin_Vadim_Calculator.cpp
@@ -235,7 +235,7 @@ TEST(Mukhin_Vadim_Konverter_and_calculator, TES) {
     std::string result5 = add("ABC", 16, "1010", 2);
     assert(result5 == "AC6");
     std::string result6 = add("10", 2, "7", 8);
-    assert(result6 == "11"); 
+    assert(result6 == "11");
     std::string result7 = add("123", 10, "12345", 10);
     assert(result7 == "12468");
     std::string result8 = add("0", 10, "0", 10);

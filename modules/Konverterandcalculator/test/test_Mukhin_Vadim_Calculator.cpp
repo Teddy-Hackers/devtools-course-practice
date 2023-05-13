6bfbe248f5a4065644ab133c0ab4e58a295ad618
NArKEe-ken <84380106+NArKEe-ken@users.noreply.github.com>
2023-05-13 14:58:00 +0300

    commit

--- modules/Konverterandcalculator/test/test_Mukhin_Vadim_Calculator.cpp
+++ modules/Konverterandcalculator/test/test_Mukhin_Vadim_Calculator.cpp
@@ -6,46 +6,46 @@
 #include "include/Converter.h"
 
 TEST(Mukhin_Vadim_Konverter_and_calculator, TEST_SUM) {
-    std::string num1 = "10";
+    std::String num1 = "10";
     int base1 = 10;
-    std::string num2 = "1";
+    std::String num2 = "1";
     int base2 = 10;
     // slovo
-    std::string sum = add(num1, base1, num2, base2);
+    std::String sum = add(num1, base1, num2, base2);
     EXPECT_EQ(sum, "11");
 }
 
 TEST(Mukhin_Vadim_Konverter_and_calculator, TEST_DIFF) {
-    std::string num1 = "10";
+    std::String num1 = "10";
     int base1 = 10;
-    std::string num2 = "1";
+    std::String num2 = "1";
     int base2 = 10;
-    std::string diff = subtract(num1, base1, num2, base2);
+    std::String diff = subtract(num1, base1, num2, base2);
     EXPECT_EQ(diff, "9");
 }
 
 TEST(Mukhin_Vadim_Konverter_and_calculator, TEST_PRODUCT) {
-    std::string num1 = "10";
+    std::String num1 = "10";
     int base1 = 10;
-    std::string num2 = "1";
+    std::String num2 = "1";
     int base2 = 10;
-    std::string product = multiply(num1, base1, num2, base2);
+    std::String product = multiply(num1, base1, num2, base2);
     EXPECT_EQ(product, "10");
 }
 
 TEST(Mukhin_Vadim_Konverter_and_calculator, TEST_DIV) {
-    std::string num1 = "10";
+    std::String num1 = "10";
     int base1 = 10;
-    std::string num2 = "1";
+    std::String num2 = "1";
     int base2 = 10;
-    std::string quotient = divide(num1, base1, num2, base2);
+    std::String quotient = divide(num1, base1, num2, base2);
     EXPECT_EQ(quotient, "10");
 }
 
 TEST(Mukhin_Vadim_Konverter_and_calculator, TEST_CONVERTER) {
-    std::string num = "101001";
+    std::String num = "101001";
     int base1 = 2;
     int base2 = 10;
-    std::string result = convert(num, base1, base2);
+    std::String result = convert(num, base1, base2);
     EXPECT_EQ(result, "41");
 }

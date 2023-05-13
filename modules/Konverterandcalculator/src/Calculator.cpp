f72c32acd90e227ac2d1213a3867760be69ea2c6
NArKEe-ken <84380106+NArKEe-ken@users.noreply.github.com>
2023-05-13 18:18:42 +0300

    commit

--- modules/Konverterandcalculator/src/Calculator.cpp
+++ modules/Konverterandcalculator/src/Calculator.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <string>
 #include <cmath>
-
+#include <vector>
 std::string add(std::string num1, int base1, std::string num2, int base2) {
     // Преобразуем числа из их систем счисления в десятичную систему
     int decimal1 = 0, decimal2 = 0;
@@ -51,103 +51,92 @@ std::string add(std::string num1, int base1, std::string num2, int base2) {
 std::string subtract(std::string num1, int base1, std::string num2, int base2) {
     int decimal1 = 0, decimal2 = 0;
 
-    if (base1 == 10) {
-        decimal1 = stoi(num1);
-    } else {
-        for (int i = num1.length() - 1; i >= 0; i--) {
-            int digit;
-            if (num1[i] >= '0' && num1[i] <= '9')
+    for (int i = num1.length() - 1; i >= 0; i--) {
+        int digit;
+        if (num1[i] >= '0' && num1[i] <= '9') {
             digit = num1[i] - '0';
-            else if (num1[i] >= 'A' && num1[i] <= 'F')
+        } else if (num1[i] >= 'A' && num1[i] <= 'Z') {
             digit = num1[i] - 'A' + 10;
-            else if (num1[i] >= 'a' && num1[i] <= 'f')
+        } else if (num1[i] >= 'a' && num1[i] <= 'z') {
             digit = num1[i] - 'a' + 10;
-            decimal1 += digit * pow(base1, num1.length() - 1 - i);
         }
+        decimal1 += digit * pow(base1, num1.length() - 1 - i);
     }
 
-    if (base2 == 10) {
-        decimal2 = stoi(num2);
-    } else {
-        for (int i = num2.length() - 1; i >= 0; i--) {
-            int digit;
-            if (num2[i] >= '0' && num2[i] <= '9')
+    for (int i = num2.length() - 1; i >= 0; i--) {
+        int digit;
+        if (num2[i] >= '0' && num2[i] <= '9') {
             digit = num2[i] - '0';
-            else if (num2[i] >= 'A' && num2[i] <= 'F')
+        } else if (num2[i] >= 'A' && num2[i] <= 'Z') {
             digit = num2[i] - 'A' + 10;
-            else if (num2[i] >= 'a' && num2[i] <= 'f')
+        } else if (num2[i] >= 'a' && num2[i] <= 'z') {
             digit = num2[i] - 'a' + 10;
-            decimal2 += digit * pow(base2, num2.length() - 1 - i);
         }
+        decimal2 += digit * pow(base2, num2.length() - 1 - i);
     }
 
     int diffDecimal = decimal1 - decimal2;
+
+    if (diffDecimal < 0) {
+        return "-" + subtract(num2, base2, num1, base1);
+    }
 
     std::string diff = "";
 
     while (diffDecimal > 0) {
         int digit = diffDecimal % base1;
         char c;
-        if (digit >= 0 && digit <= 9) c = digit + '0';
-        else
-        c = digit - 10 + 'A';
+        if (digit >= 0 && digit <= 9) {
+            c = digit + '0';
+        } else {
+            c = digit - 10 + 'A';
+        }
 
         diff = c + diff;
         diffDecimal /= base1;
+    }
+
+    if (diff.empty()) {
+        return "0";
     }
 
     return diff;
 }
 
 std::string multiply(std::string num1, int base1, std::string num2, int base2) {
-    int decimal1 = 0, decimal2 = 0;
+    std::vector<int> product(num1.length() + num2.length(), 0);
 
-    if (base1 == 10) {
-        decimal1 = stoi(num1);
-    } else {
-        for (int i = num1.length() - 1; i >= 0; i--) {
-            int digit;
-            if (num1[i] >= '0' && num1[i] <= '9')
-            digit = num1[i] - '0';
-            else if (num1[i] >= 'A' && num1[i] <= 'F')
-            digit = num1[i] - 'A' + 10;
-            else if (num1[i] >= 'a' && num1[i] <= 'f')
-            digit = num1[i] - 'a' + 10;
-            decimal1 += digit * pow(base1, num1.length() - 1 - i);
+    for (int i = num1.length() - 1; i >= 0; i--) {
+        int carry = 0;
+        int digit1 = (num1[i] >= '0' && num1[i] <= '9') ? num1[i] - '0' : num1[i] - 'A' + 10;
+
+        for (int j = num2.length() - 1; j >= 0; j--) {
+            int digit2 = (num2[j] >= '0' && num2[j] <= '9') ? num2[j] - '0' : num2[j] - 'A' + 10;
+            int productDigit = digit1 * digit2 + carry + product[i + j + 1];
+            carry = productDigit / base1;
+            product[i + j + 1] = productDigit % base1;
         }
+
+        product[i] += carry;
     }
 
-    if (base2 == 10) {
-        decimal2 = stoi(num2);
-    } else {
-        for (int i = num2.length() - 1; i >= 0; i--) {
-            int digit;
-            if (num2[i] >= '0' && num2[i] <= '9')
-            digit = num2[i] - '0';
-            else if (num2[i] >= 'A' && num2[i] <= 'F')
-            digit = num2[i] - 'A' + 10;
-            else if (num2[i] >= 'a' && num2[i] <= 'f')
-            digit = num2[i] - 'a' + 10;
-            decimal2 += digit * pow(base2, num2.length() - 1 - i);
-        }
+    int i = 0;
+    while (i < product.size() && product[i] == 0) {
+        i++;
     }
 
-    int productDecimal = decimal1 * decimal2;
-
-    std::string product = "";
-
-    while (productDecimal > 0) {
-        int digit = productDecimal % base1;
-        char c;
-        if (digit >= 0 && digit <= 9) c = digit + '0';
-        else
-        c = digit - 10 + 'A';
-
-        product = c + product;
-        productDecimal /= base1;
+    if (i == product.size()) {
+        return "0";
     }
 
-    return product;
+    std::string result = "";
+    while (i < product.size()) {
+        char c = (product[i] >= 0 && product[i] <= 9) ? product[i] + '0' : product[i] - 10 + 'A';
+        result += c;
+        i++;
+    }
+
+    return result;
 }
 
 std::string divide(std::string num1, int base1, std::string num2, int base2) {

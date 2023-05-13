2e32f87dc9ac801caf1d4d5aa82789bdd420969a
NArKEe-ken <84380106+NArKEe-ken@users.noreply.github.com>
2023-05-13 15:48:38 +0300

    commit

--- modules/Konverterandcalculator/test/test_Mukhin_Vadim_Calculator.cpp
+++ modules/Konverterandcalculator/test/test_Mukhin_Vadim_Calculator.cpp
@@ -6,40 +6,40 @@
 #include "include/Converter.h"
 
 TEST(Mukhin_Vadim_Konverter_and_calculator, TEST_SUM) {
-    std::string num1 = "10";
-    int base1 = 10;
-    std::string num2 = "1";
-    int base2 = 10;
+    std::string num1 = "1010";
+    int base1 = 2;
+    std::string num2 = "FF";
+    int base2 = 16;
     // slovo
     std::string sum = add(num1, base1, num2, base2);
-    EXPECT_EQ(sum, "11");
+    EXPECT_EQ(sum, "100001001");
 }
 
 TEST(Mukhin_Vadim_Konverter_and_calculator, TEST_DIFF) {
-    std::string num1 = "10";
-    int base1 = 10;
-    std::string num2 = "1";
+    std::string num1 = "f";
+    int base1 = 16;
+    std::string num2 = "7";
     int base2 = 10;
     std::string diff = subtract(num1, base1, num2, base2);
-    EXPECT_EQ(diff, "9");
+    EXPECT_EQ(diff, "8");
 }
 
 TEST(Mukhin_Vadim_Konverter_and_calculator, TEST_PRODUCT) {
-    std::string num1 = "10";
-    int base1 = 10;
-    std::string num2 = "1";
-    int base2 = 10;
+    std::string num1 = "4";
+    int base1 = 16;
+    std::string num2 = "6";
+    int base2 = 8;
     std::string product = multiply(num1, base1, num2, base2);
-    EXPECT_EQ(product, "10");
+    EXPECT_EQ(product, "18");
 }
 
 TEST(Mukhin_Vadim_Konverter_and_calculator, TEST_DIV) {
-    std::string num1 = "10";
-    int base1 = 10;
+    std::string num1 = "f";
+    int base1 = 16;
     std::string num2 = "1";
     int base2 = 10;
     std::string quotient = divide(num1, base1, num2, base2);
-    EXPECT_EQ(quotient, "10");
+    EXPECT_EQ(quotient, "F");
 }
 
 TEST(Mukhin_Vadim_Konverter_and_calculator, TEST_CONVERTER) {

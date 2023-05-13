a93a95a70269cd4ef320ac97b42fa7a510798f16
NArKEe-ken <84380106+NArKEe-ken@users.noreply.github.com>
2023-05-13 14:52:42 +0300

    commit

--- modules/Konverterandcalculator/include/Calculator.h
+++ modules/Konverterandcalculator/include/Calculator.h
@@ -3,8 +3,8 @@
 #define MODULES_KONVERTERANDCALCULATOR_INCLUDE_CALCULATOR_H_
 #include <iostream>
 #include <string>
-string add(string num1, int base1, string num2, int base2);
-string subtract(string num1, int base1, string num2, int base2);
-string multiply(string num1, int base1, string num2, int base2);
-string divide(string num1, int base1, string num2, int base2);
+std::string add(std::string num1, int base1, std::string num2, int base2);
+std::string subtract(std::string num1, int base1, std::string num2, int base2);
+std::string multiply(std::string num1, int base1, std::string num2, int base2);
+std::string divide(std::string num1, int base1, std::string num2, int base2);
 #endif  // MODULES_KONVERTERANDCALCULATOR_INCLUDE_CALCULATOR_H_

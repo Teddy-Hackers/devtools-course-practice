82c7918e99b3501e91ae21f3a6b3713fb1d1ff1a
NArKEe-ken <84380106+NArKEe-ken@users.noreply.github.com>
2023-05-13 15:55:56 +0300

    commit

--- modules/Konverterandcalculator/test/test_Mukhin_Vadim_Calculator.cpp
+++ modules/Konverterandcalculator/test/test_Mukhin_Vadim_Calculator.cpp
@@ -26,20 +26,20 @@ TEST(Mukhin_Vadim_Konverter_and_calculator, TEST_DIFF) {
 
 TEST(Mukhin_Vadim_Konverter_and_calculator, TEST_PRODUCT) {
     std::string num1 = "4";
-    int base1 = 16;
+    int base1 = 10;
     std::string num2 = "6";
     int base2 = 8;
     std::string product = multiply(num1, base1, num2, base2);
-    EXPECT_EQ(product, "18");
+    EXPECT_EQ(product, "24");
 }
 
 TEST(Mukhin_Vadim_Konverter_and_calculator, TEST_DIV) {
     std::string num1 = "f";
-    int base1 = 16;
-    std::string num2 = "1";
-    int base2 = 10;
+    int base1 = 8;
+    std::string num2 = "10100";
+    int base2 = 2;
     std::string quotient = divide(num1, base1, num2, base2);
-    EXPECT_EQ(quotient, "F");
+    EXPECT_EQ(quotient, "3");
 }
 
 TEST(Mukhin_Vadim_Konverter_and_calculator, TEST_CONVERTER) {

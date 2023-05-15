342b418b023759cffae678c28558fd154ab1ec25
382008-3 Лебединский Илья <iluxa00701@gmail.com>
2023-05-15 16:43:23 +0300

    fix3

--- modules/complex-number/test/test_Lebedinskiy_Ilya_complex_number.cpp
+++ modules/complex-number/test/test_Lebedinskiy_Ilya_complex_number.cpp
@@ -12,8 +12,8 @@ TEST(Lebedinskiy_Ilya_ComplexNumberTest, Can_Sum) {
 	ComplexNumber a(re1, im1);
 	ComplexNumber b(re2, im2);
 	ComplexNumber z = a + b;
-	EXPECT_EQ(9, z.getRe());
-	EXPECT_EQ(7, z.getIm());
+	ASSERT_EQ(9, z.getRe());
+	ASSERT_EQ(7, z.getIm());
 }
 
 TEST(Lebedinskiy_Ilya_ComplexNumberTest, Can_Min) {
@@ -24,45 +24,14 @@ TEST(Lebedinskiy_Ilya_ComplexNumberTest, Can_Min) {
 	ComplexNumber a(re1, im1);
 	ComplexNumber b(re2, im2);
 	ComplexNumber z = a - b;
-	EXPECT_EQ(5, z.getRe());
-	EXPECT_EQ(6, z.getIm());
+	ASSERT_EQ(5, z.getRe());
+	ASSERT_EQ(6, z.getIm());
 }
 
 TEST(Lebedinskiy_Ilya_ComplexNumberTest, Can_Num) {
 	double re = 15.0;
 	double im = 7.0;
 	ComplexNumber z(re, im);
-	EXPECT_EQ(z.getRe(), 15.0);
-	EXPECT_EQ(z.getIm(), 7.0);
-}
-
-TEST(Lebedinskiy_Ilya_ComplexNumberTest, Can_Copy) {
-	double re = 9.0;
-	double im = 5.0;
-	ComplexNumber z1(re, im);
-	ComplexNumber z2(z1);
-	EXPECT_EQ(z1.getRe(), z2.getRe());
-	EXPECT_EQ(z1.getIm(), z2.getIm());
-}
-
-TEST(Lebedinskiy_Ilya_ComplexNumberTest, Can_Multiply) {
-	double re1 = 4.0;
-	double im1 = 2.0;
-	double re2 = 6.0;
-	double im2 = 1.0;
-	ComplexNumber c1(re1, im1);
-	ComplexNumber c2(re2, im2);
-	ComplexNumber res = c2 * c1;
-	EXPECT_DOUBLE_EQ(22.0, res.getRe());
-	EXPECT_DOUBLE_EQ(16.0, res.getIm());
-}
-
-TEST(Lebedinskiy_Ilya_ComplexNumberTest, Can_Compare) {
-	double re1 = 2.0;
-	double im1 = 0.1;
-	double re2 = 2.0;
-	double im2 = 0.1;
-	ComplexNumber z1(re1, im1);
-	ComplexNumber z2(re2, im2);
-	EXPECT_TRUE(z1 == z2);
+	ASSERT_EQ(z.getRe(), 15.0);
+	ASSERT_EQ(z.getIm(), 7.0);
 }

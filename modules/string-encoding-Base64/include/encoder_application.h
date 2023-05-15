b2d715fe3c8f0102a82c5aa16b548cce9991cb56
Evgeny <mousedead21@gmail.com>
2023-05-15 19:46:46 +0300

    Update encoder_application.h

--- modules/string-encoding-Base64/include/encoder_application.h
+++ modules/string-encoding-Base64/include/encoder_application.h
@@ -5,15 +5,21 @@
 #include <string>
 
 class EncoderApplication {
-public: EncoderApplication() =
-    default;
-    EncoderApplication(const EncoderApplication & ca) = delete;
-    EncoderApplication(EncoderApplication && ca) = delete;
-    std::string operator()(int argc,
-                           const char ** argv);
+ public:
+    EncoderApplication() =default;
 
-private: void help(const char * appname);
+    EncoderApplication(const EncoderApplication &ca) = delete;
+
+    EncoderApplication(EncoderApplication &&ca) = delete;
+
+    std::string operator()(int argc,
+                           const char **argv);
+
+ private:
+    void help(const char *appname);
+
     bool validateArguments(int argc,
-                           const char ** argv);
+                           const char **argv);
+
     std::string m_message;
 };

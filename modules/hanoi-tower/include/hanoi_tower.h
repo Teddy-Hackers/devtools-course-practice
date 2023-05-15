35cfc7e2c009a3f67c6cdd732d3eba4b59901fb3
wwoffxx <84740926+wwoffxx@users.noreply.github.com>
2023-05-15 22:43:57 +0300

    initial

---
+++ modules/hanoi-tower/include/hanoi_tower.h
@@ -0,0 +1,11 @@
+// Copyright 2023 Binko Alexander
+
+#ifndef MODULES_HANOI_TOWER_INCLUDE_HANOI_TOWER_H_
+#define MODULES_HANOI_TOWER_INCLUDE_HANOI_TOWER_H_
+
+#include <iostream>
+#include <string>
+
+void hanoi(int n, int from_rod, int to_rod, int aux_rod);
+
+#endif  // MODULES_HANOI_TOWER_INCLUDE_HANOI_TOWER_H_
---
+++ modules/hanoi-tower/src/hanoi_tower.cpp
@@ -0,0 +1,38 @@
+#include <../include/hanoi_tower.h>
+
+#include <iostream>
+
+static int counter = 0;
+
+void hanoi(int n, int from_rod, int to_rod, int aux_rod) {
+  if (from_rod < 4 && from_rod > 0 && to_rod < 4 && to_rod > 0 && aux_rod < 4 &&
+      aux_rod > 0) {
+    if (from_rod == to_rod) {
+      throw std::string("'from rod' and 'to rod' is equal");
+    }
+    if (from_rod == aux_rod) {
+      throw std::string("'from rod' and 'aux rod' is equal");
+    }
+    if (to_rod == aux_rod) {
+      throw std::string("'aux rod' and 'to rod' is equal");
+    }
+    if (n == 1) {
+      return;
+    }
+    counter++;
+    hanoi(n - 1, from_rod, aux_rod, to_rod);
+    hanoi(n - 1, aux_rod, to_rod, from_rod);
+  }
+  throw std::string(
+      "'from rod'/'to rod'/'aux rod' - must be in the range from 1 to 3 "
+      "inclusive");
+}
+
+int tower_solve(int n, int from_rod, int to_rod, int aux_rod) {
+  try {
+    hanoi(n, from_rod, to_rod, aux_rod);
+  } catch (std::string str) {
+    throw std::string(str);
+  }
+  return counter;
+}

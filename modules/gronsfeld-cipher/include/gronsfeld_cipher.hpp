216ccc7cc2c32952021765583839762d045b0d7f
Roman Mitin <ramonmitin290502@yandex.ru>
2023-05-16 18:34:25 +0300

    Add test and class definition

---
+++ modules/gronsfeld-cipher/include/gronsfeld_cipher.hpp
@@ -0,0 +1,18 @@
+// Copyright 2023 Mitin Roman
+#include <vector>
+#include <string>
+#include <cinttypes>
+
+class gronsfeld_cipher_t
+{
+    constexpr static uint8_t alphabet_size = 'z' - 'a' + 1;
+
+    std::vector<uint8_t> key;
+
+    std::string code_impl(const std::string to_code, bool is_code);
+public:
+    gronsfeld_cipher_t(const std::vector<uint8_t>& new_key);
+    
+    std::string code(const std::string to_code);
+    std::string decode(const std::string to_decode);
+};

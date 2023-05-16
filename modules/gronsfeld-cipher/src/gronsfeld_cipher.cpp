0b290330b678c564a58707299ae6b4ec86fc0987
Roman Mitin <ramonmitin290502@yandex.ru>
2023-05-16 21:06:39 +0300

    Partial fix code style error 4

--- modules/gronsfeld-cipher/src/gronsfeld_cipher.cpp
+++ modules/gronsfeld-cipher/src/gronsfeld_cipher.cpp
@@ -4,22 +4,23 @@
 #include <exception>
 
 gronsfeld_cipher_t::gronsfeld_cipher_t(const std::vector<uint8_t> &new_key)
-    : key(new_key){
+    : key(new_key) {
     for (size_t i = 0; i < key.size(); i++)
         if (key[i] > alphabet_size)
             throw std::exception{};
 }
 
-std::string gronsfeld_cipher_t::code_impl(const std::string& to_code, 
+std::string gronsfeld_cipher_t::code_impl(const std::string& to_code,
     bool is_code) {
     size_t ksize = key.size();
     std::string res;
     res.resize(to_code.size());
     int sign = is_code ? 1 : -1;
 
-    for( size_t i = 0; i < to_code.size(); i++) {
-        char letter = ((to_code[i] - 'a') + sign * key[i % ksize]) % alphabet_size;
-        if(letter < 0)
+    for (size_t i = 0; i < to_code.size(); i++) {
+        char letter = to_code[i] - 'a';
+        letter = (letter + sign * key[i % ksize]) % alphabet_size;
+        if (letter < 0)
             letter += alphabet_size;
         assert(letter >= 0 && letter < alphabet_size);
         res[i] = letter + 'a';
@@ -35,4 +36,3 @@ std::string gronsfeld_cipher_t::code(const std::string& to_code) {
 std::string gronsfeld_cipher_t::decode(const std::string& to_decode) {
     return code_impl(to_decode, false);
 }
-    

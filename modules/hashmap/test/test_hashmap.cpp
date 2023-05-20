e09e1b579b47f6ac847d8d2865100e00aefe6594
Artem Shipilov <hugo.nieto@mail.ru>
2023-05-20 21:28:25 +0300

    new tests added

--- modules/hashmap/test/test_hashmap.cpp
+++ modules/hashmap/test/test_hashmap.cpp
@@ -8,57 +8,98 @@ TEST(Hashmap, Can_Create_An_Empty_Hashmap) {
     ASSERT_ANY_THROW(hashmap THash(0));
 }
 
-TEST(Hashmap, Can_Create_Hashmap) { ASSERT_NO_THROW(hashmap THash(5)); }
+TEST(Hashmap, Can_Create_Hashmap) {
+    ASSERT_NO_THROW(hashmap THash(5));
+}
 
-TEST(Hashmap, Can_Copy) {
+TEST(Hashmap, Can_Copy_Empty_Hashmap) {
     hashmap THash(5);
     ASSERT_NO_THROW(hashmap THash2(THash));
 }
 
-TEST(Hashmap, GetKey_From_Empty_Hashtable) {
+TEST(Hashmap, Can_Copy_Hashmap) {
+    hashmap THash(2);
+    THash.InsRecord("5", 10);
+    THash.InsRecord("2", 11);
+    ASSERT_NO_THROW(hashmap THash2(THash));
+}
+
+TEST(Hashmap, GetKey_From_Empty_Hashmap) {
     hashmap THash(2);
     ASSERT_ANY_THROW(THash.GetKey());
 }
 
-TEST(Hashmap, GetKey_NoThrow) {
+TEST(Hashmap, GetKey_Search_Key_Hashmap) {
     hashmap THash(2);
     THash.InsRecord("2", 20);
     ASSERT_NO_THROW(THash.GetKey());
 }
 
-TEST(Hashmap, GetKey_True) {
+TEST(Hashmap, GetKey_Search_Key_NotEmpty_Hashmap) {
     hashmap THash(4);
     THash.InsRecord("5", 10);
     THash.InsRecord("2", 11);
     ASSERT_EQ(THash.GetKey(), "2");
 }
 
-TEST(Hashmap, GetData_From_Empty_Hashtable) {
+TEST(Hashmap, GetKey_Last_Pos) {
+    hashmap THash(3);
+    THash.InsRecord("5", 10);
+    THash.InsRecord("2", 11);
+    THash.InsRecord("1", 12);
+    ASSERT_NO_THROW(THash.GetKey(2));
+}
+
+TEST(Hashmap, GetKey_First_Pos) {
+    hashmap THash(3);
+    THash.InsRecord("5", 10);
+    THash.InsRecord("2", 11);
+    THash.InsRecord("1", 12);
+    ASSERT_NO_THROW(THash.GetKey(0));
+}
+
+TEST(Hashmap, GetData_From_Empty_Hashmap) {
     hashmap THash(2);
     ASSERT_ANY_THROW(THash.GetData());
 }
 
-TEST(Hashmap, GetData_NoThrow) {
+TEST(Hashmap, GetData_Search_Data_Hashmap) {
     hashmap THash(2);
     THash.InsRecord("2", 20);
     ASSERT_NO_THROW(THash.GetData());
 }
 
-TEST(Hashmap, GetData_True) {
+TEST(Hashmap, GetData_Search_Data_NotEmpty_Hashmap) {
     hashmap THash(4);
     THash.InsRecord("5", 10);
     THash.InsRecord("2", 11);
     ASSERT_EQ(*THash.GetData(), 11);
 }
 
-TEST(Hashmap, GoNext_Throw) {
+TEST(Hashmap, GetData_Last_Pos) {
+    hashmap THash(4);
+    THash.InsRecord("5", 10);
+    THash.InsRecord("2", 11);
+    THash.InsRecord("1", 12);
+    ASSERT_NO_THROW(*THash.GetData(2));
+}
+
+TEST(Hashmap, GetData_First_Pos) {
+    hashmap THash(4);
+    THash.InsRecord("5", 10);
+    THash.InsRecord("2", 11);
+    THash.InsRecord("1", 12);
+    ASSERT_NO_THROW(*THash.GetData(0));
+}
+
+TEST(Hashmap, GoNext_In_Hashmap) {
     hashmap THash(3);
     THash.InsRecord("5", 10);
     THash.InsRecord("2", 11);
     ASSERT_NO_THROW(THash.GoNext());
 }
 
-TEST(Hashmap, Move_On_When_The_Hashtable_Ends) {
+TEST(Hashmap, Move_On_When_The_Hashmap_Ends) {
     hashmap THash(2);
     THash.InsRecord("5", 10);
     THash.InsRecord("2", 11);
@@ -67,40 +108,40 @@ TEST(Hashmap, Move_On_When_The_Hashtable_Ends) {
     ASSERT_ANY_THROW(THash.GoNext());
 }
 
-TEST(Hashmap, Reset_Throw) {
+TEST(Hashmap, Reset_In_Hashmap) {
     hashmap THash(3);
     THash.InsRecord("5", 10);
     THash.InsRecord("2", 11);
     ASSERT_NO_THROW(THash.Reset());
 }
 
-TEST(Hashmap, HashFunc_Throw) {
+TEST(Hashmap, HashFunc_In_Hashmap) {
     hashmap THash(3);
     THash.InsRecord("5", 10);
     THash.InsRecord("2", 11);
     ASSERT_NO_THROW(THash.HashFunc("5"));
 }
 
-TEST(Hashmap, InsRecord_NoThrow) {
+TEST(Hashmap, InsRecord_In_Hashmap) {
     hashmap TH(2);
     ASSERT_NO_THROW(TH.InsRecord("4", 12));
 }
 
-TEST(Hashmap, Insert_Into_Completed_Hashtable) {
+TEST(Hashmap, Insert_Into_Completed_Hashmap) {
     hashmap THash(2);
     THash.InsRecord("5", 20);
     THash.InsRecord("6", 21);
     ASSERT_ANY_THROW(THash.InsRecord("7", 22));
 }
 
-TEST(Hashmap, Insert_Hashtable_Of_Existing_Entry) {
+TEST(Hashmap, Insert_Hashmap_Of_Existing_Entry) {
     hashmap THash(2);
     THash.InsRecord("5", 20);
     THash.InsRecord("6", 21);
     ASSERT_ANY_THROW(THash.InsRecord("6", 21));
 }
 
-TEST(Hashmap, Insert_Into_Almost_Complete_Hashtable) {
+TEST(Hashmap, Insert_Into_Almost_Complete_Hashmap) {
     hashmap THash(3);
     THash.InsRecord("5", 12);
     THash.InsRecord("1", 3);
@@ -128,7 +169,7 @@ TEST(Hashmap, Delete_An_Inserted_Record) {
     ASSERT_NO_THROW(THash.DelRecord("2"));
 }
 
-TEST(Hashmap, Delete_From_Full_Hashtable) {
+TEST(Hashmap, Delete_From_Full_Hashmap) {
     hashmap THash(3);
     THash.InsRecord("1", 1);
     THash.InsRecord("2", 2);
@@ -136,7 +177,7 @@ TEST(Hashmap, Delete_From_Full_Hashtable) {
     ASSERT_NO_THROW(THash.DelRecord("1"));
 }
 
-TEST(Hashmap, Delete_From_Empty_Hashtable) {
+TEST(Hashmap, Delete_From_Empty_Hashmap) {
     hashmap THash(2);
     ASSERT_ANY_THROW(THash.DelRecord("1"));
 }
@@ -164,18 +205,18 @@ TEST(Hashmap, DelRecord_Value) {
     ASSERT_EQ(THash.GetDataCount(), 3);
 }
 
-TEST(Hashmap, FindRecord_An_Entry_In_The_Hashtable) {
+TEST(Hashmap, FindRecord_An_Entry_In_The_Hashmap) {
     hashmap THash(2);
     THash.InsRecord("10", 1);
     ASSERT_NO_THROW(THash.FindRecord("10"));
 }
 
-TEST(Hashmap, FindRecord_An_Entry_In_Empty_Hashtable1) {
+TEST(Hashmap, FindRecord_An_Entry_In_Empty_Hashmap) {
     hashmap THash(2);
     ASSERT_NO_THROW(THash.FindRecord("1"));
 }
 
-TEST(Hashmap, FindRecord_For_An_Entry_In_NonEmpty_Hashtable) {
+TEST(Hashmap, FindRecord_For_An_Entry_In_NonEmpty_Hashmap) {
     hashmap THash(5);
     THash.InsRecord("5", 3);
     THash.InsRecord("4", 4);
@@ -183,12 +224,12 @@ TEST(Hashmap, FindRecord_For_An_Entry_In_NonEmpty_Hashtable) {
     ASSERT_NO_THROW(THash.FindRecord("4"));
 }
 
-TEST(Hashmap, FindRecord_For_Empty_record_In_Hashtable) {
+TEST(Hashmap, FindRecord_For_Empty_record_In_Hashmap) {
     hashmap THash(2);
     ASSERT_EQ(THash.FindRecord("1"), nullptr);
 }
 
-TEST(Hashmap, FindRecord_For_record_In_Hashtable) {
+TEST(Hashmap, FindRecord_For_record_In_Hashmap) {
     hashmap THash(5);
     THash.InsRecord("1", 1);
     THash.InsRecord("2", 5);

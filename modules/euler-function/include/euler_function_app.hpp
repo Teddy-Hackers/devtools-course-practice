57793b6d8867ac9cec5fd99df8487d3074bfd9ff
Roman Mitin <ramonmitin290502@yandex.ru>
2023-05-21 09:22:08 +0300

    New tests

--- modules/euler-function/include/euler_function_app.hpp
+++ modules/euler-function/include/euler_function_app.hpp
@@ -4,11 +4,11 @@
 #include <cinttypes>
 
 class application_t {
+ public:
     static void print_help();
     static std::vector<int64_t> parse(int argc, const char* const* argv);
     static int calc_and_print_result(std::vector<int64_t> num_to_calc);
 
- public:
     application_t() = default;
 
     int run(int argc, const char* const * argv);
--- modules/euler-function/test/test_euler_function_app.cpp
+++ modules/euler-function/test/test_euler_function_app.cpp
@@ -17,12 +17,22 @@ TEST(Euler_function_app, test_run_function) {
     EXPECT_EQ(app.run(1, NULL), 0);
 }
 
-TEST(Euler_function_app, test_negative_val) {
+TEST(Euler_function_app, test_not_a_number_in_input) {
+    // Arange
+    application_t app{};
+    const char* args[2] = {"some_sting", "afsadfg-23dada"};
+
+
+    // Act and Assert
+    EXPECT_ANY_THROW(application_t::parse(2, args));
+}
+
+TEST(Euler_function_app, test_calc_and_print_result) {
     // Arange
     application_t app{};
     const char* args[2] = {"some_sting", "-2"};
 
 
     // Act and Assert
-    EXPECT_EQ(app.run(2, args), 1);
+    EXPECT_EQ(application_t::calc_and_print_result({2}), 0);
 }

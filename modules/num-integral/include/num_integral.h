4ce32dc4c283d8d78cebb0a5c9030b2e75eda772
TibidoX <vovaboch0@gmail.com>
2023-05-21 17:17:35 +0300

    style_fix2

--- modules/num-integral/include/num_integral.h
+++ modules/num-integral/include/num_integral.h
@@ -10,13 +10,14 @@
 
 class NumIntegral {
  private:
-
  public:
-	 static double integral1D(double a, double b, int n, const std::function<double(double)>& fun);
-	 static double integral2D(double a, double b, double c, double d,
-	 int n, const std::function<double(double, double)>& fun);
-	 static double integral3D(double a, double b, double c, double d,
-	 double e, double f, int n, const std::function<double(double, double, double)>& fun);
+     static double integral1D(double a, double b, int n,
+     const std::function<double(double)>& fun);
+     static double integral2D(double a, double b, double c, double d,
+     int n, const std::function<double(double, double)>& fun);
+     static double integral3D(double a, double b, double c, double d,
+	 double e, double f, int n,
+     const std::function<double(double, double, double)>& fun);
 };
 
 #endif  // MODULES_NUM-INTEGRAL_INCLUDE_NUM_INTEGRAL_H_

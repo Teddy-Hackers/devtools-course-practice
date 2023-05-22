3288cdbc62be18ccfbc801c597da4c5758814468
StJi <denproko@yandex.ru>
2023-05-22 23:42:55 +0300

    upd

--- modules/rev_pol_not/include/lex.h
+++ modules/rev_pol_not/include/lex.h
@@ -1,7 +1,7 @@
 // Copyright 2023 Prokofev Denis
 
-#ifndef  MODULES_PROKOFEV_REV_POL_NOT_INCLUDE_LEX_H_
-#define  MODULES_PROKOFEV_REV_POL_NOT_INCLUDE_LEX_H_
+#ifndef  MODULES_REV_POL_NOT_INCLUDE_LEX_H_
+#define  MODULES_REV_POL_NOT_INCLUDE_LEX_H_
 
 #include <string>
 
@@ -28,4 +28,4 @@ class Lex {
     std::string getS() { return s; }
 };
 
-#endif  //  MODULES_PROKOFEV_REV_POL_NOT_INCLUDE_LEX_H_
+#endif  //  MODULES_REV_POL_NOT_INCLUDE_LEX_H_

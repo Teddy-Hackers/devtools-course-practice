60fc2fd566630a6c6591b9eb1387ad04ac28168e
StJi <denproko@yandex.ru>
2023-05-22 23:49:03 +0300

    1

--- modules/rev_pol_not/include/pol_not.h
+++ modules/rev_pol_not/include/pol_not.h
@@ -1,8 +1,8 @@
 // Copyright 2023 Prokofev Denis
 
 
-#ifndef  MODULES_REV_POL_NOT_INCLUDE_POL_NOT_H_  
-#define  MODULES_REV_POL_NOT_INCLUDE_POL_NOT_H_  
+#ifndef  MODULES_REV_POL_NOT_INCLUDE_POL_NOT_H_
+#define  MODULES_REV_POL_NOT_INCLUDE_POL_NOT_H_
 
 #include <string>
 #include "include/lex.h"
@@ -15,4 +15,4 @@ class PolNot {
     TQueue<Lex*>* sEOL(std::string _s);
     std::string revPolNot(TQueue<Lex*>* q);
 };
-#endif  //  MODULES_REV_POL_NOT_INCLUDE_POL_NOT_H_  
+#endif  //  MODULES_REV_POL_NOT_INCLUDE_POL_NOT_H_

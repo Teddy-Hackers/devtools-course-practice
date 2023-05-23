89c3ac51e970d3e341a44ebb2e8dd158cb8bd252
StJi <denproko@yandex.ru>
2023-05-23 23:24:31 +0300

    Update lex.h

--- modules/rev_pol_not/include/lex.h
+++ modules/rev_pol_not/include/lex.h
@@ -10,4 +10,13 @@ enum State {
     q0, q1
 };
 
+class Lex {
+ public:
+    Lex() {}
+    Lex(std::string _s, Type _t, int _val);
+    Type getType();
+    int getVal();
+    std::string getS();
+};
+
 #endif  //  MODULES_REV_POL_NOT_INCLUDE_LEX_H_

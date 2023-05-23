e16873754199b9e99a76ca568a8e00eff3dd37f6
StJi <denproko@yandex.ru>
2023-05-23 16:41:03 +0300

    Update test_pol_not.cpp

--- modules/rev_pol_not/test/test_pol_not.cpp
+++ modules/rev_pol_not/test/test_pol_not.cpp
@@ -153,7 +153,7 @@ TEST(PolNot, polnot_zero_divide) {
     PolNot l1;
     TQueue<Lex*>* lex = new TQueue<Lex*>;
     std::string str = "2/0";
-    ASSERT_THROW(l1.sEOL(str));
+    ASSERT_THROW(l1.sEOL(str), "divide zero");
 }
 
 

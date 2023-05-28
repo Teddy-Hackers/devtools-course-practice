3c55f19836e49b7e0554c40b4bdb412a8872e6e8
Vladimir <aleksandrvlad63@gmail.com>
2023-05-28 14:46:58 +0300

    my six comm

--- modules/KonverterFR/src/Converter.cpp
+++ modules/KonverterFR/src/Converter.cpp
@@ -8,9 +8,9 @@
 std::string toRoman(int number) {
   const int arabicValues[] = {1000, 900, 500, 400, 100, 90, 50,
                               40,   10,  9,   5,   4,   1};
-  const string romanValues[] = {"M",  "CM", "D",  "CD", "C",  "XC", "L",
+  const std::string romanValues[] = {"M",  "CM", "D",  "CD", "C",  "XC", "L",
                                 "XL", "X",  "IX", "V",  "IV", "I"};
-  string result = "";
+  std::string result = "";
   for (int i = 0; i < 13; i++) {
     while (number >= arabicValues[i]) {
       result += romanValues[i];

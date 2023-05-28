ae4f69f6a93f1c9fcc73f9323ef10216b8c617a3
Meseyoshi <jr_lion@mail.ru>
2023-05-28 18:00:44 +0300

    Update test_matrix.cpp

--- modules/Matrix/test/test_matrix.cpp
+++ modules/Matrix/test/test_matrix.cpp
@@ -132,7 +132,7 @@ TEST(matrix, reverse_matrix) {
     a[1][0] = 2;
     a[1][1] = 5;
     matrix A(a, 2, 2);
-    matrix B(A);
+    matrix B = A;
     B.getMatrix()[0][0] = 1;
     B.getMatrix()[0][1] = -0.4;
     B.getMatrix()[1][0] = -0.4;
@@ -145,13 +145,32 @@ TEST(matrix, reverse_matrix) {
 }
 TEST(matrix, determinant) {
     double** a = new double* [2];
+    double** b = new double* [1];
+    double** c = new double* [3];
+    b[0] = new double[1];
+    b[0][0] = 1;
     for (int i = 0; i < 2; i++) {
         a[i] = new double[2];
+    }
+    for (int i = 0; i < 3; i++) {
+        c[i] = new double[3];
     }
     a[0][0] = 1;
     a[0][1] = 2;
     a[1][0] = 2;
     a[1][1] = 5;
+    c[0][0] = 1;
+    c[0][1] = 2;
+    c[0][2] = 3;
+    c[1][0] = 2;
+    c[1][1] = 3;
+    c[1][2] = 4;
+    c[2][0] = 3;
+    c[2][1] = 4;
+    c[2][2] = 5;
     matrix A(a, 2, 2);
+    matrix B(b, 1, 1);
     EXPECT_EQ(1, A.determinant());
+    EXPECT_EQ(1, B.determinant());
+    EXPECT_EQ(0, C.determinant());
 }

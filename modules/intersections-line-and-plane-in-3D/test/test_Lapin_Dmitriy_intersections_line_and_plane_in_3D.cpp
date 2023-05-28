7344f6d4782b17f851d2bcb86f617dcea1f7f030
dlapin2014 <dmitriylapin2001@gmail.com>
2023-05-28 17:58:31 +0300

    fix problems

--- modules/intersections-line-and-plane-in-3D/test/test_Lapin_Dmitriy_intersections_line_and_plane_in_3D.cpp
+++ modules/intersections-line-and-plane-in-3D/test/test_Lapin_Dmitriy_intersections_line_and_plane_in_3D.cpp
@@ -5,7 +5,7 @@
 
 #include "include/intersections_line_and_plane.h"
 
-TEST(Lapin_Dmitriy_IntersectionTest, Intersection_test) {
+TEST(Lapin_Dmitriy_IntersectionTest, Intersection_test_one) {
     std::vector<double>plane = { 3, 2, 3, 4 };
     std::vector<double>dot = { 1, 2, 1 };
     std::vector<double>direction = { 1, 2, 3 };
@@ -16,6 +16,19 @@ TEST(Lapin_Dmitriy_IntersectionTest, Intersection_test) {
     EXPECT_EQ(result.second[0], 0.125);
     EXPECT_EQ(result.second[1], 0.25);
     EXPECT_EQ(result.second[2], -1.625);
+}
+
+TEST(Lapin_Dmitriy_IntersectionTest, Intersection_test_two) {
+    std::vector<double>plane = { 0, 0, 1, 1 };
+    std::vector<double>dot = { 1, 1, 0 };
+    std::vector<double>direction = { 0, 0, 1 };
+    std::pair<int,
+        std::vector<double>> result = isIntersection(plane, dot, direction);
+
+    EXPECT_EQ(result.first, 0);
+    EXPECT_EQ(result.second[0], 1);
+    EXPECT_EQ(result.second[1], 1);
+    EXPECT_EQ(result.second[2], -1);
 }
 
 

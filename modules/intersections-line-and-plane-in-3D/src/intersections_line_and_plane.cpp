69b4d2eb4cd3cb50b9766054cf0063cd00b0e11a
dlapin2014 <dmitriylapin2001@gmail.com>
2023-05-28 11:35:05 +0300

    LapinSecondLab

--- modules/intersections-line-and-plane-in-3D/src/intersections_line_and_plane.cpp
+++ modules/intersections-line-and-plane-in-3D/src/intersections_line_and_plane.cpp
@@ -5,7 +5,7 @@
 #include<vector>
 std::pair<__int8, std::vector<double>> isIntersection(std::vector<double> plane,
     std::vector<double> dot, std::vector<double> direction) {
-    double A = plane[0]*dot[0] + plane[1] * dot[1] + plane[2] * dot[2] + plane[3];
+    double A = plane[0]*dot[0]+plane[1]*dot[1]+plane[2]*dot[2]+plane[3];
     double B = plane[0]*direction[0]+plane[1]*direction[1]+plane[2]*direction[2];
     if (B != 0) {
         // a straight line intersects a plane
@@ -15,13 +15,15 @@ std::pair<__int8, std::vector<double>> isIntersection(std::vector<double> plane,
         double z = dot[2] + t * direction[2];
         std::vector<double> result = { x, y, z };
         return std::pair<__int8, std::vector<double>>(0, result);
-    }else {
+    } else {
         if (A == 0) {
             // a straight line belongs a plane
-            return std::pair<__int8, std::vector<double>>(1, std::vector<double>(0));
-        }else {
+            return std::pair<__int8,
+                std::vector<double>>(1, std::vector<double>(0));
+        } else {
             // a straight line parallel a plane
-            return std::pair<__int8, std::vector<double>>(2, std::vector<double>(0));
+            return std::pair<__int8,
+                std::vector<double>>(2, std::vector<double>(0));
         }
     }
     return std::pair<__int8,

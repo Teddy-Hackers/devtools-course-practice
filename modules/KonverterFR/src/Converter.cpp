9765dc600a51b8df286dfd878f6faa9c2959bfc5
Vladimir <aleksandrvlad63@gmail.com>
2023-05-28 14:51:59 +0300

    my seven comm

--- modules/KonverterFR/src/Converter.cpp
+++ modules/KonverterFR/src/Converter.cpp
@@ -1,4 +1,4 @@
-﻿  // Copyright 2023 Francuzov Maksim
+﻿// Copyright 2023 Francuzov Maksim
 #include <iostream>
 #include <map>
 #include <string>
@@ -9,7 +9,7 @@ std::string toRoman(int number) {
   const int arabicValues[] = {1000, 900, 500, 400, 100, 90, 50,
                               40,   10,  9,   5,   4,   1};
   const std::string romanValues[] = {"M",  "CM", "D",  "CD", "C",  "XC", "L",
-                                "XL", "X",  "IX", "V",  "IV", "I"};
+                                     "XL", "X",  "IX", "V",  "IV", "I"};
   std::string result = "";
   for (int i = 0; i < 13; i++) {
     while (number >= arabicValues[i]) {
@@ -21,8 +21,9 @@ std::string toRoman(int number) {
 }
 
 int toArabic(std::string roman) {
-  map<char, int> romanValues = {{'M', 1000}, {'D', 500}, {'C', 100}, {'L', 50},
-                                {'X', 10},   {'V', 5},   {'I', 1}};
+  std::map<char, int> romanValues = {{'M', 1000}, {'D', 500}, {'C', 100},
+                                     {'L', 50},   {'X', 10},  {'V', 5},
+                                     {'I', 1}};
   int result = 0;
   for (int i = 0; i < roman.length(); i++) {
     if (i > 0 && romanValues[roman[i]] > romanValues[roman[i - 1]]) {
@@ -36,12 +37,12 @@ int toArabic(std::string roman) {
 
 std::string checkInputType(std::string input) {
   for (char &c : input) {
-    if (!isdigit(c) && c != 'I' && c != 'V' && c != 'X' && c != 'L' &&
+    if (!std::isdigit(c) && c != 'I' && c != 'V' && c != 'X' && c != 'L' &&
         c != 'C' && c != 'D' && c != 'M') {
       return "Invalid input";
     }
   }
-  if (isdigit(input[0])) {
+  if (std::isdigit(input[0])) {
     return "Arabic";
   } else {
     return "Roman";

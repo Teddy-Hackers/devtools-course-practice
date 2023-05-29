ee2b3fcd216f81aeb79d8738d13f512a0f57f8af
Anastasya <antonova0506@icloud.com>
2023-05-29 18:02:05 +0300

    add new tests

--- modules/BigInt/test/test_bigInt.cpp
+++ modules/BigInt/test/test_bigInt.cpp
@@ -10,6 +10,83 @@ TEST(BigIntConstructor, LargePositiveNumber) {
     EXPECT_EQ(a.digits, "123456789012345678901234567890");
 }
 
+TEST(BigIntTest, DefaultConstructorTest) {
+    BigInt a;
+    EXPECT_EQ(a.to_string(), "0");
+}
+
+TEST(BigIntTest, IntConstructorTest) {
+    BigInt a(123);
+    EXPECT_EQ(a.to_string(), "123");
+}
+
+TEST(BigIntTest, StringConstructorTest) {
+    BigInt a("12345678901234567890");
+    EXPECT_EQ(a.to_string(), "12345678901234567890");
+}
+
+TEST(BigIntTest, AdditionTest) {
+    BigInt a("99999999999999999999");
+    BigInt b("1");
+    BigInt c("100000000000000000000");
+    EXPECT_EQ(a + b, c);
+}
+
+TEST(BigIntTest, SubtractionTest) {
+    BigInt a("100000000000000000000");
+    BigInt b("1");
+    BigInt c("99999999999999999999");
+    EXPECT_EQ(a - b, c);
+}
+
+TEST(BigIntTest, MultiplicationTest) {
+    BigInt a("99999999999999999999");
+    BigInt b("2");
+    BigInt c("199999999999999999998");
+    EXPECT_EQ(a * b, c);
+}
+
+TEST(BigIntTest, DivisionTest) {
+    BigInt a("99999999999999999999");
+    BigInt b("2");
+    BigInt c("50000000000000000000");
+    EXPECT_EQ(a / b, c);
+}
+
+TEST(BigIntTest, ModulusTest) {
+    BigInt a("99999999999999999999");
+    BigInt b("2");
+    BigInt c("1");
+    EXPECT_EQ(a % b, c);
+}
+
+TEST(BigIntTest, GreaterThanTest) {
+    BigInt a("99999999999999999999");
+    BigInt b("1");
+    EXPECT_TRUE(a > b);
+    EXPECT_FALSE(b > a);
+}
+
+TEST(BigIntTest, GreaterThanOrEqualToTest) {
+    BigInt a("99999999999999999999");
+    BigInt b("1");
+    EXPECT_TRUE(a >= b);
+    EXPECT_TRUE(a >= a);
+    EXPECT_FALSE(b >= a);
+}
+
+TEST(BigIntTest, LessThanTest) {
+    BigInt a("99999999999999999999");
+    BigInt b("100000000000000000000");
+    EXPECT_TRUE(a < b);
+    EXPECT_FALSE(b < a);
+}
+
+TEST(BigIntTest, ToStringTest) {
+    BigInt a("-99999999999999999999");
+    EXPECT_EQ(a.to_string(), "-99999999999999999999");
+}
+
 TEST(BigIntConstructor, LargeNegativeNumber) {
     BigInt a("-123456789012345678901234567890");
     EXPECT_EQ(a.is_negative, true);

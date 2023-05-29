a7527bc813c670b8d916a2d7be0585e5b66ccb13
Anastasya <antonova0506@icloud.com>
2023-05-29 18:36:19 +0300

    add new tests

--- modules/BigInt/test/test_bigInt.cpp
+++ modules/BigInt/test/test_bigInt.cpp
@@ -23,13 +23,6 @@ TEST(BigIntTest, AdditionTest) {
     EXPECT_EQ(a + b, c);
 }
 
-TEST(BigIntTest, SubtractionTest) {
-    BigInt a("100000000000000000000");
-    BigInt b("1");
-    BigInt c("99999999999999999999");
-    EXPECT_EQ(a - b, c);
-}
-
 TEST(BigIntTest, MultiplicationTest) {
     BigInt a("99999999999999999999");
     BigInt b("2");
@@ -44,9 +37,24 @@ TEST(BigIntTest, DivisionTest) {
     EXPECT_EQ(a / b, c);
 }
 
-TEST(BigIntTest, ModulusTest) {
+TEST(BigIntTest, GreaterThanTest) {
     BigInt a("99999999999999999999");
-    BigInt b("2");
-    BigInt c("1");
-    EXPECT_EQ(a % b, c);
+    BigInt b("1");
+    EXPECT_TRUE(a > b);
+    EXPECT_FALSE(b > a);
+}
+
+TEST(BigIntTest, GreaterThanOrEqualToTest) {
+    BigInt a("99999999999999999999");
+    BigInt b("1");
+    EXPECT_TRUE(a >= b);
+    EXPECT_TRUE(a >= a);
+    EXPECT_FALSE(b >= a);
+}
+
+TEST(BigIntTest, LessThanTest) {
+    BigInt a("99999999999999999999");
+    BigInt b("100000000000000000000");
+    EXPECT_TRUE(a < b);
+    EXPECT_FALSE(b < a);
 }

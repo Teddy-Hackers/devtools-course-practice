bc2a51fb4ffe62cd395e5e7dbbd6c6afcaee90b9
Anastasya <antonova0506@icloud.com>
2023-05-29 19:47:09 +0300

    add new twst

--- modules/BigInt/test/test_bigInt.cpp
+++ modules/BigInt/test/test_bigInt.cpp
@@ -30,7 +30,7 @@ TEST(BigIntTest, MultiplicationTest) {
     EXPECT_EQ(a * b, c);
 }
 
-TEST(BigIntTest, DivisionTest) {
+TEST(BigIntTest, DivisionTest) { 
     BigInt a("12345678901234567890");
     BigInt b("99999");
     BigInt c("123456789012345");
@@ -40,73 +40,69 @@ TEST(BigIntTest, DivisionTest) {
 }
 
 TEST(BigIntTest, GreaterThanOperatorTest) {
-    BigInt a("123");
-    BigInt b("45");
-    BigInt c("-123");
+    BigInt a("123456789012345678901234567890");
+    BigInt b("98765432109876543210987654321");
+    BigInt c("-123456789012345678901234567890");
 
-    EXPECT_TRUE(b > a);
-    EXPECT_FALSE(a > b);
-    EXPECT_FALSE(c > a);
+    EXPECT_TRUE(c < b);
     EXPECT_TRUE(a > c);
-    EXPECT_FALSE(c > b);
+    EXPECT_FALSE(b < a);
+    EXPECT_FALSE(b < c);
 }
 
-TEST(BigIntTest, GreaterThanOrEqualToOperatorTest) {
-    BigInt a("123");
-    BigInt b("123");
-    BigInt c("-123");
+TEST(BigIntTest, GreaterThanOrEqualToOperatorTest) { 
+    BigInt a("1364763897649287634876836487");
+    BigInt b("1364763897649287634876836487");
+    BigInt c("-1364763897649287634876836487");
 
     EXPECT_TRUE(b >= a);
-    EXPECT_FALSE(a >= b);
+    EXPECT_TRUE(a >= b);
     EXPECT_TRUE(a >= c);
     EXPECT_FALSE(c >= a);
     EXPECT_TRUE(b >= b);
 }
 
 TEST(BigIntTest, LessThanOperatorTest) {
-    BigInt a("123");
-    BigInt b("45");
-    BigInt c("-123");
+    BigInt a("123456789012345678901234567890");
+    BigInt b("98765432109876543210987654321");
+    BigInt c("-123456789012345678901234567890");
 
-    EXPECT_TRUE(b < a);
-    EXPECT_FALSE(a < b);
+    EXPECT_TRUE(b > a);
+    EXPECT_FALSE(a > b);
     EXPECT_TRUE(c < a);
     EXPECT_FALSE(a < c);
-    EXPECT_FALSE(c < c);
 }
 
-TEST(BigIntTest, LessThanOrEqualToOperatorTest) {
-    BigInt a("123");
-    BigInt b("123");
-    BigInt c("-123");
+TEST(BigIntTest, LessThanOrEqualToOperatorTest) { 
+    BigInt a("123456789012345678901234567890");
+    BigInt b("98765432109876543210987654321");
+    BigInt c("-123456789012345678901234567890");
 
-    EXPECT_TRUE(b <= a);
+    EXPECT_TRUE(b >= a);
     EXPECT_FALSE(a <= b);
     EXPECT_TRUE(c <= a);
     EXPECT_FALSE(a <= c);
-    EXPECT_TRUE(b <= b);
 }
 
 TEST(BigIntTest, EqualityOperatorTest) {
-    BigInt a("123");
-    BigInt b("-123");
-    BigInt c("456");
-    BigInt d("123");
+    BigInt a("123456789012345678901234567890");
+    BigInt b("-123456789012345678901234567890");
+    BigInt c("123456789012345678901234567890");
 
-    EXPECT_TRUE(a == d);
+    EXPECT_TRUE(a == a);
+    EXPECT_TRUE(a == c);
     EXPECT_FALSE(a == b);
     EXPECT_FALSE(b == c);
-    EXPECT_FALSE(a == c);
 }
 
 TEST(BigIntTest, NotEqualityOperatorTest) {
-    BigInt a("123");
-    BigInt b("-123");
-    BigInt c("456");
-    BigInt d("123");
+    BigInt a("123456789012345678901234567890");
+    BigInt b("-123456789012345678901234567890");
+    BigInt c("123456789012345678901234567890");
 
-    EXPECT_FALSE(a != d);
+    EXPECT_FALSE(a != a);
+    EXPECT_FALSE(a != c);
     EXPECT_TRUE(a != b);
     EXPECT_TRUE(b != c);
-    EXPECT_TRUE(a != c);
+}
 }

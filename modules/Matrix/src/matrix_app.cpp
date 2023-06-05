035c670720ce1e7f9313e7c8a740404b84a3382a
Пашина Алина <nice.alina28@mail.ru>
2023-06-05 19:58:01 +0300

    commit 10

--- modules/Matrix/src/matrix_app.cpp
+++ modules/Matrix/src/matrix_app.cpp
@@ -16,19 +16,13 @@ std::string MatrixAPP::operator()(int argc, char** argv) {
     return "Need more arguments";
   }
   std::string str_result = "";
-  std::cout << "was in op";
   arguments args = param_parsing(argc, argv);
-  std::cout << "did parsing in op";
   matrix res;
   if (args.operation == "plus") {
     res = args.m1 + args.m2;
     for (int i = 0; i < res.getRows(); i++) {
       for (int j = 0; j < res.getColumns(); j++) {
-        if (i == 0 && j == 0) {
-          str_result = str_result + std::to_string(res.getMatrix()[i][j]);
-        } else {
-          str_result = str_result + "," + std::to_string(res.getMatrix()[i][j]);
-        }
+        str_result = str_result + std::to_string(res.getMatrix()[i][j]);
       }
     }
   }
@@ -46,8 +40,8 @@ MatrixAPP::arguments MatrixAPP::param_parsing(int argc, char** argv) {
   double** mm1 = new double*[size1];
   double** mm2 = new double*[size2];
 
-  std::cout << "was in parsing" std::string dot = argv[3];
-  auto delimeter;
+  std::string dot = argv[3];
+  auto delimeter = dot.find(",");
   for (int i = 0; i < size1 * size1 - 1; i++) {
     delimeter = dot.find(",");
     mm1[static_cast<int>(i / size1)][static_cast<int>(i % size1)] =
@@ -55,7 +49,8 @@ MatrixAPP::arguments MatrixAPP::param_parsing(int argc, char** argv) {
   }
   mm1[size1 - 1][size1 - 1] = std::stoi(dot.substr(delimeter + 1));
   args.m1 = matrix(mm1, size1, size1);
-  std::cout << "was in 1 parsing" std::string dot2 = argv[4];
+  std::cout << "was in parsing" << std::endl;
+  std::string dot2 = argv[4];
   for (int i = 0; i < size2 * size2 - 1; i++) {
     delimeter = dot2.find(",");
     mm2[static_cast<int>(i / size2)][static_cast<int>(i % size2)] =

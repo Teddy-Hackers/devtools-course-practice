92c206b1a1574660fc90e53b5c87696ac286f7d5
Evgeniy <eugeneshuin@yandex.ru>
2023-06-09 01:14:38 +0300

    fixing_style_2

--- modules/complex-number/test/test_Shuin Evgeniy_complex_number.cpp
+++ modules/complex-number/test/test_Shuin Evgeniy_complex_number.cpp
@@ -18,7 +18,7 @@ EXPECT_EQ(3.7, comp.getRe());
 EXPECT_EQ(1.8, comp.getIm());
 }
 
-TEST(Shuin_Evgeniy_tests, can_copy_a_number) {
+TEST(Shuin_Evgeniy_tests, can_copy_a_complex_number) {
 ComplexNumber comp(3.7, 1.8);
 ComplexNumber comp2(comp);
 
@@ -26,7 +26,7 @@ EXPECT_EQ(comp2.getRe(), comp.getRe());
 EXPECT_EQ(comp2.getIm(), comp.getIm());
 }
 
-TEST(Shuin_Evgeniy_tests, can_assign) {
+TEST(Shuin_Evgeniy_tests, can_assign_complex_number_to_another) {
 ComplexNumber comp(3.7, 1.8);
 ComplexNumber comp2;
 comp2 = comp;
@@ -35,7 +35,7 @@ EXPECT_EQ(comp2.getRe(), comp.getRe());
 EXPECT_EQ(comp2.getIm(), comp.getIm());
 }
 
-TEST(Shuin_Evgeniy_tests, can_sum) {
+TEST(Shuin_Evgeniy_tests, can_sum_two_complex_numbers) {
 ComplexNumber comp(-1.5, 2.9);
 ComplexNumber comp2(3, -0.5);
 ComplexNumber comp3;

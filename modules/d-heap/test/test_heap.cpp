871d7d5c35a9cc36ede0eca7ee99345f66cac2c6
qooju <84714244+qooju@users.noreply.github.com>
2023-06-10 22:09:42 +0300

    Update test_heap.cpp

--- modules/d-heap/test/test_heap.cpp
+++ modules/d-heap/test/test_heap.cpp
@@ -3,39 +3,39 @@
 #include <gtest/gtest.h>
 #include "include/DHeap.h"
 
-TEST(miheev_ivan_dheap, can_call_constructor_with_default_param) {
+TEST(test_dheap, can_call_constructor_with_default_param) {
   // Assert
   ASSERT_NO_THROW(DHeap());
 }
 
-TEST(miheev_ivan_dheap, can_call_constructor_with_input_param) {
+TEST(test_dheap, can_call_constructor_with_input_param) {
   // Assert
   ASSERT_NO_THROW(DHeap(15, 6));
 }
 
-TEST(miheev_ivan_dheap, dary_larger_than_capacity) {
+TEST(test_dheap, dary_larger_than_capacity) {
   // Assert
   ASSERT_NO_THROW(DHeap(5, 20));
 }
 
-TEST(miheev_ivan_dheap, can_create_heap_with_wrong_dary) {
+TEST(test_dheap, can_create_heap_with_wrong_dary) {
   // Assert
   ASSERT_ANY_THROW(DHeap(3, -2));
 }
 
-TEST(miheev_ivan_dheap, can_create_heap_with_wrong_capacity) {
+TEST(test_dheap, can_create_heap_with_wrong_capacity) {
   // Assert
   ASSERT_ANY_THROW(DHeap(-5, 2));
 }
 
-TEST(miheev_ivan_dheap, empty_test) {
+TEST(test_dheap, empty_test) {
   // Arrange & Act
   DHeap hp;
   // Assert
   ASSERT_NO_THROW(hp.isEmpty());
 }
 
-TEST(miheev_ivan_dheap, not_empty_test) {
+TEST(test_dheap, not_empty_test) {
   // Arrange & Act
   DHeap hp;
   hp.insert(1);
@@ -43,7 +43,7 @@ TEST(miheev_ivan_dheap, not_empty_test) {
   ASSERT_EQ(0, hp.isEmpty());
 }
 
-TEST(miheev_ivan_dheap, full_test) {
+TEST(test_dheap, full_test) {
   // Arrange & Act
   DHeap hp;
   hp.insert(1);
@@ -52,7 +52,7 @@ TEST(miheev_ivan_dheap, full_test) {
   ASSERT_NO_THROW(hp.isFull());
 }
 
-TEST(miheev_ivan_dheap, not_full_test) {
+TEST(test_dheap, not_full_test) {
   // Arrange & Act
   DHeap hp;
 
@@ -60,14 +60,14 @@ TEST(miheev_ivan_dheap, not_full_test) {
   ASSERT_EQ(0, hp.isFull());
 }
 
-TEST(miheev_ivan_dheap, insert_test) {
+TEST(test_dheap, insert_test) {
   // Arrange & Act
   DHeap hp;
   // Assert
   ASSERT_NO_THROW(hp.insert(0));
 }
 
-TEST(miheev_ivan_dheap, insert_multiply_values_test) {
+TEST(test_dheap, insert_multiply_values_test) {
   // Arrange & Act
   DHeap hp;
   for (int i = -4; i < 0; i++) hp.insert(i);
@@ -75,7 +75,7 @@ TEST(miheev_ivan_dheap, insert_multiply_values_test) {
   ASSERT_NO_THROW(hp.insert(5));
 }
 
-TEST(miheev_ivan_dheap, extract_min_test) {
+TEST(test_dheap, extract_min_test) {
   // Arrange & Act
   DHeap hp;
   for (int i = 4; i > 0; i--) hp.insert(i);
@@ -84,7 +84,7 @@ TEST(miheev_ivan_dheap, extract_min_test) {
   ASSERT_EQ(-5, hp.extractMin());
 }
 
-TEST(miheev_ivan_dheap, extract_min_with_equal_values_test) {
+TEST(test_dheap, extract_min_with_equal_values_test) {
   // Arrange & Act
   DHeap hp;
   for (int i = 4; i > 0; i--) hp.insert(1);
@@ -92,7 +92,7 @@ TEST(miheev_ivan_dheap, extract_min_with_equal_values_test) {
   ASSERT_EQ(1, hp.extractMin());
 }
 
-TEST(miheev_ivan_dheap, copy_constructor_equality_test) {
+TEST(test_dheap, copy_constructor_equality_test) {
   // Arrange & Act
   DHeap hp(4, 3);
   DHeap cpyhp(hp);
@@ -102,7 +102,7 @@ TEST(miheev_ivan_dheap, copy_constructor_equality_test) {
   }
 }
 
-TEST(miheev_ivan_dheap, extract_decreased_value_test) {
+TEST(test_dheap, extract_decreased_value_test) {
   // Arrange & Act
   DHeap hp;
   for (int i = 10; i > 0; i--) hp.insert(i);
@@ -111,7 +111,14 @@ TEST(miheev_ivan_dheap, extract_decreased_value_test) {
     ASSERT_EQ(-5, hp.extractMin());
 }
 
-TEST(miheev_ivan_dheap, throw_empty_test) {
+TEST(test_dheap, copy_constructor_test) {
+  // Arrange & Act
+  DHeap hp(4, 3);
+  // Assert
+  ASSERT_NO_THROW(DHeap(hp));
+}
+
+TEST(test_dheap, throw_empty_test) {
   // Arrange & Act
   DHeap hp;
 

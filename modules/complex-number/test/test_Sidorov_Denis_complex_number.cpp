2a88b6d423d81fd279e82943a1e536db1a485ef5
Denis <53953397+SiDenBro@users.noreply.github.com>
2023-06-10 13:08:59 +0300

    Lab1fix3

--- modules/complex-number/test/test_Sidorov_Denis_complex_number.cpp
+++ modules/complex-number/test/test_Sidorov_Denis_complex_number.cpp
@@ -4,21 +4,21 @@
 
 #include "include/complex_number.h"
 
-TEST(Sidorov_Denis_ComplexNumberTest, TEST_SUM1) {
+TEST(Sidorov_Denis_ComplexNumberTest, TEST_SUM) {
   ComplexNumber a(1600.0, 25.0), b(800.0, 11.0), expected(2400.0, 36.0);
   ComplexNumber result = a + b;
   ASSERT_EQ(expected, result);
 }
 
-TEST(Sidorov_Denis_ComplexNumberTest, TEST_SUM2) {
+TEST(Sidorov_Denis_ComplexNumberTest, TEST_MINUS) {
   ComplexNumber z1(50.0, 0.0);
   ComplexNumber z2(0.0, 50.0);
-  ComplexNumber expression = z1 + z2;
-  EXPECT_EQ(expression, ComplexNumber(50.0, 50.0));
+  ComplexNumber expression = z1 - z2;
+  EXPECT_EQ(expression, ComplexNumber(0.0, 0.0));
 }
 
-TEST(Sidorov_Denis_ComplexNumberTest, TEST_SUM3) {
-  ComplexNumber a(6.0, 8.0), b(2.0, 6.0), expected(8.0, 14.0);
-  ComplexNumber result = a + b;
+TEST(Sidorov_Denis_ComplexNumberTest, TEST_MULT) {
+  ComplexNumber a(6.0, 8.0), b(2.0, 6.0), expected(12.0, 48.0);
+  ComplexNumber result = a * b;
   ASSERT_EQ(result, expected);
 }

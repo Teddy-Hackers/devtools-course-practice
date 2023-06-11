0197c33ee458ff7365928010965e7ba84e166016
qooju <84714244+qooju@users.noreply.github.com>
2023-06-11 22:39:59 +0300

    Update DHeap.h

--- modules/d-heap/include/DHeap.h
+++ modules/d-heap/include/DHeap.h
@@ -33,8 +33,8 @@ class DHeap {
   }
 
   double extractMin();
-  bool isEmpty() { return size == 0; }
-  bool isFull() { return size == capacity; }
+  bool isEmpty() const { return size == 0; }
+  bool isFull() const { return size == capacity; }
   void insert(double n);
   void heapify(int idx);
   void deheapify(int idx);

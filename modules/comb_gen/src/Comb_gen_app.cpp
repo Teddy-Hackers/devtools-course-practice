c270f8885ae5e18b4ae5ea3440c63cbd4503dfa1
aleksandarsmnvc <aleksandarsmnvc@gmail.com>
2023-06-12 04:17:14 +0300

    cpplint fix2

--- modules/comb_gen/src/Comb_gen_app.cpp
+++ modules/comb_gen/src/Comb_gen_app.cpp
@@ -16,14 +16,17 @@ std::string CombGenApp::help(std::string add_message) {
     return out_message;
 }
 
-bool CombGenApp::parseInput(int argc, const char** argv, int* n, int* k, std::vector<int>* VecIn) {
+bool CombGenApp::parseInput(int argc, const char** argv,
+int* n, int* k, std::vector<int>* VecIn) {
     try {
         if (argc == 1)throw std::invalid_argument("No command line arguments!");
         *n = std::stoi(argv[1]);
         if (*n <= 0)throw std::invalid_argument("n must be positive number!");
-        if (argc - 3 != *n)throw std::invalid_argument("Number of elements in vector should be equal n!");
+        if (argc - 3 != *n)
+        throw std::invalid_argument("Number of elements in vector should be equal n!");
         *k = std::stoi(argv[2]);
-        if (*k<1 || *k>*n)throw std::invalid_argument("K should be between 1 and N!");
+        if (*k<1 || *k>*n)
+        throw std::invalid_argument("K should be between 1 and N!");
         for (int i = 0; i < *n; i++) {
             int elem = std::stoi(argv[3 + i]);
             (*VecIn).push_back(elem);
@@ -45,7 +48,8 @@ std::string CombGenApp::operator()(int argc, const char** argv) {
     comb_gen.CombGenerate();
     message += "Out:\n";
     std::vector<std::vector<int> >VecOut = comb_gen.getVecOut();
-    for (auto vectors_it = VecOut.begin(); vectors_it != VecOut.end(); vectors_it++) {
+    for (auto vectors_it = VecOut.begin();
+    vectors_it != VecOut.end(); vectors_it++) {
         for (auto it = (*vectors_it).begin(); it != (*vectors_it).end(); it++) {
             message+=std::to_string(*it)+" ";
         }

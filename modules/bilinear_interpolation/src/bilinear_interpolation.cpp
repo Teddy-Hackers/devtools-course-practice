726255fc78666cffe672af64aaa24dc5c5d7e8f6
rezchdmi <rezchdmi@fit.cvut.cz>
2023-06-12 18:06:45 +0300

    init

---
+++ modules/bilinear_interpolation/src/bilinear_interpolation.cpp
@@ -0,0 +1,70 @@
+// Copyright 2023 Rezchikov Dmitrii
+
+#include "../include/bilinear_interpolation.h"
+
+// void BilinearInterpolator::printMtx( std::vector<std::vector<int>> & src ){
+//     std::cout << "=======\n";
+//     for ( const auto & v : src ){
+//         for ( const auto & x : v){
+//             std::cout << x << " ";
+//         }
+//         std::cout << "\n";
+//     }
+//     std::cout << "=======\n";
+// }
+
+void BilinearInterpolator::findNearest(int oldVecSize, int newVecSize, int coord, int factor, int & l, int & r){
+    l = floor( (double)coord / (double)factor) * factor;
+    r = ceil( (double)coord / (double)factor) * factor;
+}
+
+void BilinearInterpolator::linearInterpolationH( std::vector<std::vector<int>> & mtx, int yCoord, int l, int r, int point){
+    if ( r == l ){
+        return;
+    }
+    double denominator = (r - l);
+    double lWeight = (r - point) / denominator,
+            rWeight = (point - l) / denominator;
+    mtx[yCoord][point] = round(mtx[yCoord][l] * lWeight + (mtx[yCoord][r] * rWeight));
+}
+void BilinearInterpolator::linearInterpolationV( std::vector<std::vector<int>> & mtx, int xCoord, int l, int r, int point){
+    
+    double denominator = (r - l);
+    double lWeight = (r - point) / denominator,
+            rWeight = (point - l) / denominator;      
+    mtx[point][xCoord] = round(mtx[l][xCoord] * lWeight + (mtx[r][xCoord] * rWeight));
+    
+}
+
+void BilinearInterpolator::bilinearInterpolation( std::vector<std::vector<int>> & oldMatrix,
+                            std::vector<std::vector<int>> & newMatrix,
+                            int yCoord, int xCoord, int factor ){
+    int l, r;
+    findNearest(oldMatrix.size(), newMatrix.size(), yCoord, factor, l, r); // nearest in y-coordinate
+
+    int l1, r1;
+    findNearest(oldMatrix[l].size(), newMatrix[l].size(), xCoord, factor, l1, r1); // nearest in x-coordinate
+    if ( l == r && l1 == r1 ){
+        return;
+    }
+    linearInterpolationH(newMatrix, l, l1, r1, xCoord);
+    if ( l != r) {
+        findNearest(oldMatrix[r].size(), newMatrix[r].size(), xCoord, factor, l1, r1); // nearest in x-coordinate
+        linearInterpolationH(newMatrix, r, l1, r1, xCoord);
+        linearInterpolationV(newMatrix, xCoord, l, r, yCoord );
+    }
+    
+}
+
+void BilinearInterpolator::bilinearInterpolation( std::vector<std::vector<int>> & oldMatrix,
+                            std::vector<std::vector<int>> & newMatrix,
+                            int factor){
+
+    for ( int i = 0; i < newMatrix.size(); ++i ){
+        for ( int j = 0; j < newMatrix[i].size(); ++j){
+            bilinearInterpolation(oldMatrix, newMatrix, i, j, factor);
+        }
+    }
+
+    // printMtx(newMatrix);
+}

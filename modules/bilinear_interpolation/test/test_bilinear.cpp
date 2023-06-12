7ff6a55e764e67daca3f58d3ea6d991eb2e3903b
rezchdmi <rezchdmi@fit.cvut.cz>
2023-06-12 19:33:54 +0300

    test fix

--- modules/bilinear_interpolation/test/test_bilinear.cpp
+++ modules/bilinear_interpolation/test/test_bilinear.cpp
@@ -20,10 +20,10 @@ TEST(Bilinear_Interpolation, Can_do_practically_linear ) {
 
 TEST(Bilinear_Interpolation, Can_do_intended) {
     /*=========================================================*/
-    // std::vector<std::vector<int>> testIn = {         {1,      2,        3 },
-    //                                                  {4,      5,        6 },
-    //                                                  {7,      8,        9 },
-    //                                                  {10,     11,       12 } };
+    // std::vector<std::vector<int>> testIn = { {1,      2,        3 },
+    //                                          {4,      5,        6 },
+    //                                          {7,      8,        9 },
+    //                                          {10,     11,       12 } };
     std::vector<std::vector<int>> testOut = {        {1,  0,  2,   0,   3 },
                                                      {0,  0,  0,   0,   0 },
                                                      {4,  0,  5,   0,   6 },
@@ -50,7 +50,7 @@ TEST(Bilinear_Interpolation, Can_do_intended_2 ) {
     // std::vector<std::vector<int>> testIn = {         {1,     3,      5 },
     //                                                  {3,     5,      7 },
     //                                                  {5,     7,      9 },
-    //                                                  {7,     9,      11 } };
+    //                                                  {7,     9,      11 }};
 
     std::vector<std::vector<int>> testOut = {        {1,  0, 3,  0,  5 },
                                                      {0,  0, 0,  0,  0 },

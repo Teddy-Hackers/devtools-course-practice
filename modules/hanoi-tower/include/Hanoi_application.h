3ccb8c93e1504555b0008236a217c67fd44edaa5
Anastasya <antonova0506@icloud.com>
2023-06-14 02:59:00 +0300

    add lab3

---
+++ modules/hanoi-tower/include/Hanoi_application.h
@@ -0,0 +1,20 @@
+// Copyright 2023 Antonova Anastasya
+
+#ifndef MODULES_HANOI_TOWER_INCLUDE_HANOI_APPLICATION_H_
+#define MODULES_HANOI_TOWER_INCLUDE_HANOI_APPLICATION_H_
+
+#include <string>
+#include <iostream>
+#include <cstdlib>
+
+class HanoiApplication {
+ public:
+  void Run(int argc, const char** argv);
+ private:
+  std::string help();
+  static int counter;
+  bool IsValid(int argc, const char** argv);
+  std::string validatorMessage;
+};
+
+#endif  // MODULES_HANOI_TOWER_INCLUDE_HANOI_APPLICATION_H_
